c9949f627f69237968a0573ee47ba8be1e3f4d70
Lean <leian1306@gmail.com>
2023-02-10 15:46:32 +0100

    src app: configure analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,24 @@
 import './globals.css'
+import { AnalyticsWrapper } from './components/analytics';
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
-      <body>{children}</body>
+      <head>
+        {/*
+          <head /> will contain the components returned by the nearest parent
+          head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+        */}
+        <title>Next.js</title>
+      </head>
+      <body>
+        {children}
+        <AnalyticsWrapper />
+      </body>
     </html>
-  )
+  );
 }
