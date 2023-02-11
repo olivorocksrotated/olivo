c92cc5eafd5625e579c6142e4b6d8dfbdfd4a08d
Lean <leian1306@gmail.com>
2023-02-11 16:35:49 +0100

    src app: small changes to try out next

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import Head from './head';
 import { AnalyticsWrapper } from './components/analytics';
 
 export default function RootLayout({
@@ -8,13 +9,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        {/*
-          <head /> will contain the components returned by the nearest parent
-          head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-        */}
-        <title>Next.js</title>
-      </head>
+      <head></head>
       <body>
         {children}
         <AnalyticsWrapper />
