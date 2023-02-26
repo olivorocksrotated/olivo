e349d243d8b3e55b4c87f3146b1236a12622170d
Nicolas Baglivo <nicolas.baglivo@gmail.com>
2023-02-26 23:25:38 +0100

    css clean up + sligthly new style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,15 +6,15 @@ import LogoutButton from './components/logout-btn';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/pages/api/auth/[...nextauth]';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
   return (
     <main className={styles.main}>
-      <div className={`flex flex-col gap-4 justify-center items-center`}>
-        <div className={`${styles.logo} font-bold text-5xl p-10`}>
-          Olivo
+      <div className={`flex flex-col gap-4 justify-center items-center ${inter.className}`}>
+        <div className={`${styles.logo} bg-gradient-to-r from-transparent via-purple-400  to-transparent rounded-md font-bold text-5xl px-20 py-5`}>
+          OLIVO
         </div>
         <div className='text-zinc-500 font-bold'>Welcome 👋 <span className='p-2'>{ session?.user?.name }</span></div>
         <LogoutButton></LogoutButton>
