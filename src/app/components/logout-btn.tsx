d2234ef2bc61a0526dc81fcfacec4239fb1d399c
Lean <leian1306@gmail.com>
2023-03-01 16:55:39 +0100

    src: first linter run

--- src/app/components/logout-btn.tsx
+++ src/app/components/logout-btn.tsx
@@ -1,7 +1,9 @@
-"use client";
+'use client';
 
 import { signOut } from 'next-auth/react';
 
 export default function LogoutButton() {
-    return <button className='px-4 py-1 rounded border border-solid border-zinc-600' onClick={() => signOut()}>Logout</button>
+    return <button className="px-4 py-1 rounded border border-solid border-zinc-600" onClick={() => {
+        return signOut();
+    }}>Logout</button>;
 }
--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,18 +1,21 @@
 import { isDevEnvironment } from '@/lib/environment';
+
 import LoginButton from '../components/login-btn';
 
-const GithubLoginButton = () => <LoginButton>Login with Github</LoginButton>;
+const GithubLoginButton = () => {
+    return <LoginButton>Login with Github</LoginButton>;
+};
 
-export default async function SignIn() {
+export default function SignIn() {
     return (
-        <div className='h-screen w-screen flex justify-center items-center'>
-          { isDevEnvironment() ?
-            <div className='flex flex-col gap-4'>
-              <LoginButton provider='credentials'>Login in Dev mode</LoginButton>
-              <GithubLoginButton></GithubLoginButton>
-            </div>:
-            <GithubLoginButton></GithubLoginButton>
-          }
+        <div className="h-screen w-screen flex justify-center items-center">
+            { isDevEnvironment() ?
+                <div className="flex flex-col gap-4">
+                    <LoginButton provider="credentials">Login in Dev mode</LoginButton>
+                    <GithubLoginButton></GithubLoginButton>
+                </div> :
+                <GithubLoginButton></GithubLoginButton>
+            }
         </div>
     );
 }
