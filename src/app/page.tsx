65bab9e05c0e94ff4816a178a665dc41ecf658fe
Nicolas Baglivo <nicolas.baglivo@gmail.com>
2023-03-02 21:52:17 +0100

    src app layout: set font to the layout so it is used in the whole page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,14 @@
-import { Inter } from '@next/font/google';
 import { getServerSession } from 'next-auth';
 
 import { authOptions } from '@/pages/api/auth/[...nextauth]';
 
 import LogoutButton from './logout-btn';
 
-const inter = Inter({ subsets: ['latin'] });
-
 export default async function Home() {
     const session = await getServerSession(authOptions);
 
     return (
-        <main className={`flex gap-4 justify-center mt-6 items-center ${ inter.className }`}>
+        <main className="flex gap-4 justify-center mt-6 items-center">
             <div className="text-zinc-500 font-bold">Welcome 👋 <span className="p-2">{ session?.user?.name }</span></div>
             <LogoutButton></LogoutButton>
         </main>
