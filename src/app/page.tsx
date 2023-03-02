9edfa7d827fcdef0fbbc88b1a9756280c52fb9a1
Nicolas Baglivo <nicolas.baglivo@gmail.com>
2023-03-02 21:49:25 +0100

    src app: add logo to signin. remove logo from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import { getServerSession } from 'next-auth';
 import { authOptions } from '@/pages/api/auth/[...nextauth]';
 
 import LogoutButton from './logout-btn';
-import styles from './page.module.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -12,15 +11,9 @@ export default async function Home() {
     const session = await getServerSession(authOptions);
 
     return (
-        <main className={styles.main}>
-            <div className={`flex flex-col gap-4 justify-center items-center ${ inter.className }`}>
-                <div className={`${ styles.logo } bg-gradient-to-r from-transparent via-purple-400  to-transparent rounded-md font-bold text-5xl px-20 py-5`}>
-                    OLIVO
-                </div>
-                <div className="text-zinc-500 font-bold">Welcome 👋 <span className="p-2">{ session?.user?.name }</span></div>
-                <LogoutButton></LogoutButton>
-            </div>
+        <main className={`flex gap-4 justify-center mt-6 items-center ${ inter.className }`}>
+            <div className="text-zinc-500 font-bold">Welcome 👋 <span className="p-2">{ session?.user?.name }</span></div>
+            <LogoutButton></LogoutButton>
         </main>
     );
 }
-
