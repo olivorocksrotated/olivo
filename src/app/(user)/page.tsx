d003ae950fb2151802a9508d68c94f109025a5da
Nicolas Baglivo <nicolas.baglivo@gmail.com>
2023-03-03 22:44:15 +0100

    src app: restructure layouts

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -2,15 +2,12 @@ import { getServerSession } from 'next-auth';
 
 import { authOptions } from '@/pages/api/auth/[...nextauth]';
 
-import LogoutButton from './logout-btn';
-
 export default async function Home() {
     const session = await getServerSession(authOptions);
 
     return (
         <main className="flex gap-4 justify-center mt-6 items-center">
-            <div className="text-zinc-500 font-bold">Welcome 👋 <span className="p-2">{ session?.user?.name }</span></div>
-            <LogoutButton></LogoutButton>
+            <div className="text-zinc-500 text-2xl font-bold"> 👋 Hey, <span className="p-2">{ session?.user?.name }</span></div>
         </main>
     );
 }
