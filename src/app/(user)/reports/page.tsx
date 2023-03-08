b59434d938e14337e08c49c4b17e2059c4f12505
Lean <leian1306@gmail.com>
2023-03-08 17:27:35 +0100

    app user reports: move the report container to a presentational component

--- src/app/(user)/reports/page.tsx
+++ src/app/(user)/reports/page.tsx
@@ -1,13 +1,15 @@
 import { getServerSession } from '@/lib/auth/session';
 import { getReportsByManager } from '@/lib/reports/get';
 
+import Report from './components/report';
+
 export default async function Reports() {
     const { user } = await getServerSession();
     const reports = await getReportsByManager(user.id);
 
     return (
         <div>
-            {reports.map((report) => <div key={report.id}>{report.name}</div>)}
+            {reports.map((report) => <Report key={report.id} report={report}/>)}
         </div>
     );
 }
