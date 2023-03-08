35c3d2b6493af911aad4e828e789f0c840b90613
Lean <leian1306@gmail.com>
2023-03-08 13:05:41 +0100

    types next auth: improve a bit the clarity of the Session interface declaration

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -3,9 +3,9 @@ import { DefaultSession } from 'next-auth';
 declare module 'next-auth' {
     // Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
     // eslint-disable-next-line no-unused-vars
-    interface Session {
-        user: {
-            id: string
-        } & DefaultSession['user']
+    interface Session extends DefaultSession {
+        user: DefaultSession['user'] & {
+            id: string;
+        }
     }
 }
