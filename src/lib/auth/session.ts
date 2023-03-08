629af86fa78c760f0b882b3a2a70cdf55f92b986
Lean <leian1306@gmail.com>
2023-03-08 13:06:38 +0100

    lib auth session: add a new function to get the server session and ensure the session is there and complete

---
+++ src/lib/auth/session.ts
@@ -0,0 +1,14 @@
+import 'server-only';
+
+import { getServerSession as nextAuthGetServerSession, Session } from 'next-auth';
+
+import { authOptions } from '@/pages/api/auth/[...nextauth]';
+
+export async function getServerSession() {
+    const session = await nextAuthGetServerSession(authOptions);
+    if (session?.user?.email) {
+        return session as Session;
+    }
+
+    throw new Error('Session is not established');
+}
