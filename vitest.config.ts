cc0d80d418342c66178f206d32d24b1a422c9a8d
Lean <leian1306@gmail.com>
2023-03-09 14:53:27 +0100

    vitest config: configure reporters and resolve aliases

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,20 @@
 import react from '@vitejs/plugin-react';
+import path from 'path';
 import { defineConfig } from 'vitest/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
     test: {
-        environment: 'jsdom'
+        environment: 'jsdom',
+        reporters: ['basic'],
+        coverage: {
+            reporter: ['text-summary']
+        }
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src')
+        }
     }
 });
