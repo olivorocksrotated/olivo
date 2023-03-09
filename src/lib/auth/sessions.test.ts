e1a2cc7380fa1b614b6e750daa88ba47afbd3ea7
Lean <leian1306@gmail.com>
2023-03-09 15:29:47 +0100

    lib auth session: add test to try vitest. Also remove server-only marker because otherwise the test runner throws an error :)

---
+++ src/lib/auth/sessions.test.ts
@@ -0,0 +1,40 @@
+import { getServerSession as nextAuthGetServerSession } from 'next-auth';
+import { afterEach, describe, expect, it, Mock, vi } from 'vitest';
+
+import { getServerSession } from './session';
+
+vi.mock('next-auth', async () => {
+    const actual = await vi.importActual('next-auth') as any;
+
+    return {
+        ...actual,
+        getServerSession: vi.fn((): null | {} => null)
+    };
+});
+
+describe('lib auth', () => {
+    describe('session', () => {
+        const nextAuthGetServerSessionMock = nextAuthGetServerSession as Mock;
+
+        afterEach(() => {
+            vi.clearAllMocks();
+        });
+
+        describe('getServerSession', () => {
+            it('should return a session if the session user has a unique identifier', async () => {
+                const expectedSession = { user: { email: 'uniqueEmail' } };
+                nextAuthGetServerSessionMock.mockResolvedValueOnce(expectedSession);
+
+                const session = await getServerSession();
+                expect(session).to.be.deep.equal(expectedSession);
+            });
+
+            it('should return an error if the session is not established', async () => {
+                const expectedError = new Error('Session is not established');
+                nextAuthGetServerSessionMock.mockResolvedValueOnce(null);
+
+                await expect(getServerSession()).rejects.toThrowError(expectedError);
+            });
+        });
+    });
+});
