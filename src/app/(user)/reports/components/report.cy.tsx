7828a10d515e565b3820d41024c0e5b44e5ef83c
Lean <leian1306@gmail.com>
2023-03-10 18:08:15 +0100

    user report component: improve the describe blocks a bit

--- src/app/(user)/reports/components/report.cy.tsx
+++ src/app/(user)/reports/components/report.cy.tsx
@@ -2,15 +2,17 @@ import React from 'react';
 
 import Report from './report';
 
-describe('<Report /> - user report component', () => {
-    const report = { id: '1', name: 'name' };
+describe('user report', () => {
+    describe('<Report />', () => {
+        const report = { id: '1', name: 'name' };
 
-    it('should render', () => {
-        cy.mount(<Report report={report} />);
-    });
+        it('should render', () => {
+            cy.mount(<Report report={report} />);
+        });
 
-    it('should display the name of the report', () => {
-        cy.mount(<Report report={report} />);
-        cy.get('[data-cy="report-name"]').should('contain', report.name);
+        it('should display the name of the report', () => {
+            cy.mount(<Report report={report} />);
+            cy.get('[data-cy="report-name"]').should('contain', report.name);
+        });
     });
 });
