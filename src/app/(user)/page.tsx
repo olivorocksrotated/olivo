a37273e307b23171bd56596b898ff2686be64574
Lean <leian1306@gmail.com>
2023-03-10 18:12:00 +0100

    user page: use the new getServerSession function

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -1,13 +1,11 @@
-import { getServerSession } from 'next-auth';
-
-import { authOptions } from '@/pages/api/auth/[...nextauth]';
+import { getServerSession } from '@/lib/auth/session';
 
 export default async function Home() {
-    const session = await getServerSession(authOptions);
+    const session = await getServerSession();
 
     return (
         <main className="flex gap-4 justify-center mt-6 items-center">
-            <div className="text-zinc-500 text-2xl font-bold"> 👋 Hey, <span className="p-2">{ session?.user?.name }</span></div>
+            <div className="text-zinc-500 text-2xl font-bold"> 👋 Hey, <span className="p-2">{ session.user.name }</span></div>
         </main>
     );
 }
