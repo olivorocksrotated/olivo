635a3a4e730c2f1872e3b77d08b51cfe0f035c2f
Lean <leian1306@gmail.com>
2023-03-10 23:55:31 +0100

    user reports component: style the report cards

--- src/app/(user)/reports/components/report.tsx
+++ src/app/(user)/reports/components/report.tsx
@@ -1,17 +1,42 @@
 'use client';
+import * as Avatar from '@radix-ui/react-avatar';
 
 interface Props {
     report: {
         id: string;
-        name: string | null;
+        name: string;
+        image: string;
     };
+}
+
+function ReportAvatar({ report }: Props) {
+    const nameAcronym = report.name.split(/\s/).reduce((response, word)=> response += word.slice(0, 1), '');
+
+    return (
+        <Avatar.Root className="rounded w-28 h-28 inline-flex items-center justify-center align-middle select-none overflow-hidden">
+            <Avatar.Image src={report.image} alt={report.name} className="w-full h-full object-cover" style={{ borderRadius: 'inherit' }}/>
+            <Avatar.Fallback>{nameAcronym}</Avatar.Fallback>
+        </Avatar.Root>
+    );
 }
 
 export default function Report({ report }: Props) {
     return (
-        <div data-cy="report-name" className="p-4 rounded bg-lime-400 w-96 min-w-fit" style={{ backgroundColor: '#a3c072' }}>
-            {report.name ?? 'No name'}
+        <div className="p-4 rounded w-96 min-w-fit flex gap-4 bg-indigo-500">
+            <div>
+                <ReportAvatar report={report} />
+            </div>
+            <div className="text-lg" data-cy="report-name">
+                {report.name || 'No name'}
+            </div>
+            <div className="flex flex-col items-end justify-between grow">
+                <div>
+                    <button>...</button>
+                </div>
+                <div>
+                    <button>Play</button>
+                </div>
+            </div>
         </div>
     );
 }
-
