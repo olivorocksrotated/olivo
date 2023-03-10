772b51a5c4eefc5d835c5af14c91c55609a07f6c
Lean <leian1306@gmail.com>
2023-03-10 22:05:19 +0100

    user reports component: add initial stuff

--- src/app/(user)/reports/components/report.tsx
+++ src/app/(user)/reports/components/report.tsx
@@ -9,7 +9,9 @@ interface Props {
 
 export default function Report({ report }: Props) {
     return (
-        <div data-cy="report-name">{report.name ?? 'No name'}</div>
+        <div data-cy="report-name" className="p-4 rounded bg-lime-400 w-96 min-w-fit" style={{ backgroundColor: '#a3c072' }}>
+            {report.name ?? 'No name'}
+        </div>
     );
 }
 
