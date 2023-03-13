44fc6b0c7b4858f76608e2473aab825cec249de2
Lean <leian1306@gmail.com>
2023-03-13 17:43:01 +0100

    app signin page: simplify the dev environment conditional

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -11,13 +11,10 @@ export default function SignIn() {
             <div className={`${ styles.logo } bg-gradient-to-r from-transparent via-purple-400 to-transparent rounded-md font-bold text-5xl px-64 py-5`}>
                 OLIVO
             </div>
-            { isDevEnvironment() ?
-                <div className="flex gap-4">
-                    <LoginButton provider="credentials">Login in Dev mode</LoginButton>
-                    <GithubLoginButton></GithubLoginButton>
-                </div> :
+            <div className="flex gap-4">
+                { isDevEnvironment() ? <LoginButton provider="credentials">Login in Dev mode</LoginButton> : <></>}
                 <GithubLoginButton></GithubLoginButton>
-            }
+            </div>
         </div>
     );
 }
