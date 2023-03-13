030ae04398f5aec5f1d8a31d609f011ec82ada6a
Lean <leian1306@gmail.com>
2023-03-13 15:19:17 +0100

    components: add a loader component

---
+++ src/app/components/loader.tsx
@@ -0,0 +1,9 @@
+export default function Loader() {
+    return (
+        <div>
+            <div className="flex h-14 w-14 items-center justify-center rounded-full bg-gradient-to-tr from-indigo-500 to-pink-500 animate-spin">
+                <div className="h-10 w-10 rounded-full bg-black"></div>
+            </div>
+        </div>
+    );
+}
