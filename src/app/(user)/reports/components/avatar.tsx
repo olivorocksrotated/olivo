94f563696395694e5e8e4b0ae665d81b8ee9ad39
Lean <leian1306@gmail.com>
2023-03-14 14:31:29 +0100

    app user report avatar component: put back the rounded class since it looks better

--- src/app/(user)/reports/components/avatar.tsx
+++ src/app/(user)/reports/components/avatar.tsx
@@ -12,7 +12,7 @@ export default function Avatar({ report }: Props) {
     const nameAcronym = report.name.split(/\s/).reduce((response, word) => response += word.slice(0, 1), '');
 
     return (
-        <UiAvatar.Root className="rounded-full w-20 h-20 inline-flex items-center justify-center align-middle select-none overflow-hidden">
+        <UiAvatar.Root className="rounded w-20 h-20 inline-flex items-center justify-center align-middle select-none overflow-hidden">
             <UiAvatar.Image src={report.image} alt={report.name} className="w-full h-full object-cover" style={{ borderRadius: 'inherit' }} />
             <UiAvatar.Fallback>{nameAcronym}</UiAvatar.Fallback>
         </UiAvatar.Root>
