9c2dec3888f6a296e679f273a89d76f9da1a26fd
Lean <leian1306@gmail.com>
2023-03-14 13:35:44 +0100

    app user report component: move the avatar to a new file

--- src/app/(user)/reports/components/report.tsx
+++ src/app/(user)/reports/components/report.tsx
@@ -1,5 +1,6 @@
 'use client';
-import * as Avatar from '@radix-ui/react-avatar';
+
+import ReportAvatar from './report-avatar';
 
 interface Props {
     report: {
@@ -7,17 +8,6 @@ interface Props {
         name: string;
         image: string;
     };
-}
-
-function ReportAvatar({ report }: Props) {
-    const nameAcronym = report.name.split(/\s/).reduce((response, word) => response += word.slice(0, 1), '');
-
-    return (
-        <Avatar.Root className="rounded-full w-20 h-20 inline-flex items-center justify-center align-middle select-none overflow-hidden">
-            <Avatar.Image src={report.image} alt={report.name} className="w-full h-full object-cover" style={{ borderRadius: 'inherit' }} />
-            <Avatar.Fallback>{nameAcronym}</Avatar.Fallback>
-        </Avatar.Root>
-    );
 }
 
 export default function Report({ report }: Props) {
