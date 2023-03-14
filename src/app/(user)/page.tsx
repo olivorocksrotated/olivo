157bc4bb6069b0f0e5a51804e4f3600022db4e8b
Lean <leian1306@gmail.com>
2023-03-14 13:20:44 +0100

    app user page: add a link to the reports page so we can better navigate there

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -1,11 +1,20 @@
+import Link from 'next/link';
+
 import { getServerSession } from '@/lib/auth/session';
+
+import Button from '../components/button';
 
 export default async function Home() {
     const session = await getServerSession();
 
     return (
-        <main className="flex gap-4 justify-center mt-6 items-center">
+        <main className="flex flex-col gap-4 justify-center mt-6 items-center">
             <div className="text-zinc-500 text-2xl font-bold"> 👋 Hey, <span className="p-2">{session.user.name}</span></div>
+            <div>
+                <Link href="/reports">
+                    <Button>Check on your reports</Button>
+                </Link>
+            </div>
         </main>
     );
 }
