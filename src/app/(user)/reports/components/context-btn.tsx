5025259f3e33ff1e666df8119687c76bc47c0bab
Lean <leian1306@gmail.com>
2023-03-14 14:08:55 +0100

    app user report components: add context button

---
+++ src/app/(user)/reports/components/context-btn.tsx
@@ -0,0 +1,9 @@
+import { BsThreeDots } from 'react-icons/bs';
+
+export default function ContextButton() {
+    return (
+        <button type="button">
+            <BsThreeDots size={25} />
+        </button>
+    );
+}
