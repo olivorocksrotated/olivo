0991034dc8a3c6625a25f24d851e9aa05b83e5f0
Lean <leian1306@gmail.com>
2023-03-15 17:37:46 +0100

    app user reports page: add a no reports view

--- src/app/(user)/reports/page.tsx
+++ src/app/(user)/reports/page.tsx
@@ -8,19 +8,27 @@ export default async function Reports() {
     const { user } = await getServerSession();
     const reports = await getReportsByManager(user.id);
 
+    const hasReports = reports.length !== 0;
+    const reportsList = (
+        <div className="bg-indigo-900 max-w-fit rounded-lg p-4">
+            <div className="text-lg mb-4 p-3 font-normal">Upcoming 1:1s</div>
+            <div className="divide-y divide-indigo-700">
+                {reports.map((report) => (
+                    <div key={report.id}>
+                        <Report report={report} />
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+    const noReports = (
+        <div>You do not have any reports yet</div>
+    );
+
     return (
         <main>
             <PageTitle text="Reports" />
-            <div className="bg-indigo-900 max-w-fit rounded-lg p-4">
-                <div className="text-lg mb-4 p-3 font-normal">Upcoming 1:1s</div>
-                <div className="divide-y divide-indigo-700">
-                    {reports.map((report) => (
-                        <div key={report.id}>
-                            <Report report={report} />
-                        </div>
-                    ))}
-                </div>
-            </div>
+            {hasReports ? reportsList : noReports}
         </main>
     );
 }
