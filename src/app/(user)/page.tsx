23bf6e52118cd791c56173d66756b35c085abcce
Lean <leian1306@gmail.com>
2023-03-15 17:22:24 +0100

    app user page: use the PageTitle component here

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -3,13 +3,14 @@ import Link from 'next/link';
 import { getServerSession } from '@/lib/auth/session';
 
 import Button from '../components/button';
+import PageTitle from '../components/page-title';
 
 export default async function Home() {
     const session = await getServerSession();
 
     return (
-        <main className="flex flex-col gap-4 justify-center mt-6 items-center">
-            <div className="text-zinc-500 text-2xl font-bold"> 👋 Hey, <span className="p-2">{session.user.name}</span></div>
+        <main>
+            <PageTitle text={`👋 Hey, ${session.user.name}`} />
             <div>
                 <Link href="/reports">
                     <Button>Check on your reports</Button>
