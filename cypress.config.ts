2eacc7f3b1da042c6939f1d6a3151e1c461fa586
Lean <leian1306@gmail.com>
2023-03-15 18:38:43 +0100

    cypress config: add options to resolve alias

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,29 @@
+import webpackPreprocessor from '@cypress/webpack-preprocessor';
 import { defineConfig } from 'cypress';
+import path from 'path';
+
 
 export default defineConfig({
     component: {
         devServer: {
             framework: 'next',
             bundler: 'webpack'
+        },
+        setupNodeEvents(on, config) {
+            const options = {
+                webpackOptions: {
+                    resolve: {
+                        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+                        alias: {
+                            '@': path.resolve(__dirname, './src')
+                        }
+                    }
+                }
+            };
+
+            on('file:preprocessor', webpackPreprocessor(options));
+
+            return config;
         }
     },
     reporter: 'progress',
