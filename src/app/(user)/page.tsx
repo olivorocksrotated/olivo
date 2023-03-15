6b3f25ce8a440070aaada5739c67cb7049c33c2e
Lean <leian1306@gmail.com>
2023-03-15 17:28:01 +0100

    app user page: use the first name function

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -1,16 +1,18 @@
 import Link from 'next/link';
 
 import { getServerSession } from '@/lib/auth/session';
+import { getFirstName } from '@/lib/reports/name';
 
 import Button from '../components/button';
 import PageTitle from '../components/page-title';
 
 export default async function Home() {
     const session = await getServerSession();
+    const firstName = getFirstName(session.user.name ?? '');
 
     return (
         <main>
-            <PageTitle text={`👋 Hey, ${session.user.name}`} />
+            <PageTitle text={`👋 Hey, ${firstName}`} />
             <div>
                 <Link href="/reports">
                     <Button>Check on your reports</Button>
