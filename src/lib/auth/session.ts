792fa9c6d3ea1093911ffe9d1124660b210a5de1
Lean <leian1306@gmail.com>
2023-03-24 15:53:53 +0100

    lib auth session: extend the function to accept req and res in order to be able to use it in endpoints

--- src/lib/auth/session.ts
+++ src/lib/auth/session.ts
@@ -1,9 +1,13 @@
+import { NextApiRequest, NextApiResponse } from 'next';
 import { getServerSession as nextAuthGetServerSession, Session } from 'next-auth';
 
 import { authOptions } from '@/pages/api/auth/[...nextauth]';
 
-export async function getServerSession() {
-    const session = await nextAuthGetServerSession(authOptions);
+export async function getServerSession(req: NextApiRequest | undefined = undefined, res: NextApiResponse<any> | undefined = undefined) {
+    const session = !!req && !!res ?
+        await nextAuthGetServerSession(req, res, authOptions) :
+        await nextAuthGetServerSession(authOptions);
+
     if (session?.user?.email) {
         return session as Session;
     }
