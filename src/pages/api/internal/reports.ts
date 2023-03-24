c795558ecc23f386db400623281a482c947f1f8e
Lean <leian1306@gmail.com>
2023-03-24 16:11:12 +0100

    pages api internal reports: use the custom getServerSession function to simplify the logic

--- src/pages/api/internal/reports.ts
+++ src/pages/api/internal/reports.ts
@@ -1,23 +1,19 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { getServerSession } from 'next-auth';
 
+import { getServerSession } from '@/lib/auth/session';
 import { createReportRelation } from '@/lib/reports/create';
 
 import prisma from '../../../lib/prisma';
-import { authOptions } from '../auth/[...nextauth]';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
-    const session = await getServerSession(
-        req, res, authOptions
-    );
-    if (session?.user) {
-        const { reportEmail } = req.body;
-        const report = await prisma.user.findUnique({ where: { email: reportEmail } });
-        if (report) {
-            await createReportRelation(session.user.id, report.id);
+    const session = await getServerSession(req, res);
+    const { reportEmail } = req.body;
+    const report = await prisma.user.findUnique({ where: { email: reportEmail } });
+    if (report) {
+        await createReportRelation(session.user.id, report.id);
 
-            return res.status(200).json({});
-        }
+        return res.status(200).json({});
     }
+
     res.status(500).json({});
 }
