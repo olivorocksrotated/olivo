833f230116f017d9dfbd97d082355c313c22e88b
Nicolas Baglivo <nicolas.baglivo@gmail.com>
2023-03-24 21:50:53 +0100

    app components button: add more visible disabled state

--- src/app/components/button.tsx
+++ src/app/components/button.tsx
@@ -4,7 +4,7 @@ type ButtonProperties = Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, 'cla
 
 export default function Button({ children, ...props }: ButtonProperties) {
     return (
-        <button type="button" {...props} className="px-4 py-1 rounded border border-solid border-zinc-60">
+        <button type="button" {...props} className="px-4 py-1 rounded border border-solid border-zinc-500 text-white disabled:opacity-50 hover:border-zinc-200 hover:text-white">
             {children}
         </button>
     );
