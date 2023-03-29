b5a47b04fbd2e725282e627c947f2e553d6ead41
Nicolas Baglivo <nicolas.baglivo@gmail.com>
2023-03-30 00:14:23 +0200

    src lib meetings get: implement logic to calculate next meeting based on meeting description format

--- src/app/(user)/components/next-meeting-card.tsx
+++ src/app/(user)/components/next-meeting-card.tsx
@@ -5,13 +5,26 @@ import { getNextMeetingByUser } from '@/lib/meetings/get';
 
 import StartButton from '../../components/start-btn';
 
-export default async function NextMeetingCard() {
-    const { user } = await getServerSession();
-    const meeting = await getNextMeetingByUser(user.id);
-
+function MeetingInfo({ meeting }: { meeting: any }) {
     const startTime = format(meeting.startDate, 'p');
     const endTime = format(meeting.startDate, 'p');
     const day = format(meeting.startDate, 'do \'of\' MMMM');
+
+    return (
+        <>
+            <div className="font-light text-sm text-slate-100">
+                {day}
+            </div>
+            <div className="font-semibold text-lg text-slate-300">
+                {`${startTime} - ${endTime} with ${meeting.report}`}
+            </div>
+        </>
+    );
+}
+
+export default async function NextMeetingCard() {
+    const { user } = await getServerSession();
+    const meeting = await getNextMeetingByUser(user.id);
 
     return (
         <div className="p-3 rounded-lg inline-flex gap-4 bg-indigo-700 shadow-sm shadow-indigo-300">
@@ -19,12 +32,7 @@ export default async function NextMeetingCard() {
                 <div className="font-extralight text-sm text-slate-100 py-3">
                     Next feedback session
                 </div>
-                <div className="font-light text-sm text-slate-100">
-                    {day}
-                </div>
-                <div className="font-semibold text-lg text-slate-300">
-                    {`${startTime} - ${endTime} with ${meeting.report}`}
-                </div>
+                {meeting ? <MeetingInfo meeting={meeting}></MeetingInfo> : <>No feedback session scheduled</>}
             </div>
             <div className="flex justify-center items-center p-10">
                 <StartButton></StartButton>
--- src/lib/meetings/get.ts
+++ src/lib/meetings/get.ts
@@ -1,8 +1,118 @@
+import { addDays, addMinutes, addMonths, addWeeks, differenceInMilliseconds, isAfter } from 'date-fns';
+
+type MeetingDescription = {
+  startDate: Date;
+  interval: string;
+  duration: number;
+  report: string;
+};
+
+type Meeting = {
+    startDate: Date;
+    endDate: Date;
+    report: string;
+};
+
+function getDate(occurrence: Date, unit: string, countNumber: number) {
+    switch (unit) {
+    case 'days':
+        return addDays(occurrence, countNumber);
+    case 'weeks':
+        return addWeeks(occurrence, countNumber);
+    case 'months':
+        return addMonths(occurrence, countNumber);
+    default:
+        throw new Error('Invalid interval unit');
+    }
+}
+
+function nextOccurrence(meetingDescription: MeetingDescription, currentDate: Date): Date | null {
+    const startDate = meetingDescription.startDate;
+
+    const [count, unit] = meetingDescription.interval.split(' ');
+    const countNumber = parseInt(count, 10);
+    const duration = meetingDescription.duration;
+
+    const occurrences: Date[] = [startDate];
+
+    if (duration) {
+        for (let i = 1; i < duration; i++) {
+            const nextDate = getDate(occurrences[i - 1], unit, countNumber);
+            occurrences.push(nextDate);
+        }
+    }
+
+    for (const date of occurrences) {
+        if (isAfter(date, currentDate)) {
+            return date;
+        }
+    }
+
+    return null;
+}
+
+function closestDateIndex(dates: Date[], currentDate: Date): number | null {
+    if (dates.length === 0) {
+        return null;
+    }
+
+    let index = 0;
+    let closest = index;
+    let minDifference = Math.abs(differenceInMilliseconds(currentDate, dates[index]));
+
+    for (const date of dates.slice(1)) {
+        index = index + 1;
+        const difference = Math.abs(differenceInMilliseconds(currentDate, date));
+        if (difference < minDifference) {
+            minDifference = difference;
+
+            closest = index;
+        }
+    }
+
+    return closest;
+}
+
+function calculateNextMeeting(meetingDescriptions: MeetingDescription[]): Meeting | null {
+    const currentDate = new Date();
+    const nextMeetingsByUser = meetingDescriptions.reduce((meetings, meetingDescription) => {
+        const nextOccurrenceForReport = nextOccurrence(meetingDescription, currentDate);
+        if (!nextOccurrenceForReport) {
+            return meetings;
+        }
+
+        return [...meetings, {
+            startDate: nextOccurrenceForReport,
+            endDate: addMinutes(nextOccurrenceForReport, meetingDescription.duration),
+            report: meetingDescription.report
+        }];
+    }, [] as Meeting[]);
+
+    const dates = nextMeetingsByUser.map(({ startDate }) => startDate);
+    const closestMeetingIndex = closestDateIndex(dates, currentDate);
+
+    return closestMeetingIndex !== null ? nextMeetingsByUser[closestMeetingIndex] : null;
+}
+
+// Example usage
+const meetingDescriptions: MeetingDescription[] = [
+    {
+        startDate: new Date('2023-03-29T14:30:00'), // Meeting starts at 14:30
+        interval: '2 weeks',
+        duration: 10,
+        report: 'Amit'
+    },
+    {
+        startDate: new Date('2023-03-29T15:30:00'), // Meeting starts at 13:30
+        interval: '1 weeks',
+        duration: 10,
+        report: 'Rafa'
+    }
+];
+
 /* eslint-disable */
 export async function getNextMeetingByUser(_userId: string) {
-    return {
-        startDate: new Date(),
-        endDate: new Date(),
-        report: 'Amit'
-    };
+    const nextM = calculateNextMeeting(meetingDescriptions);
+    console.log('next m', nextM);
+    return nextM;
 }
