c5b3253ff948b3e53ef623639958342f163dee4f
Lean <leian1306@gmail.com>
2023-04-02 23:38:06 +0200

    user commitments actions component: add a popover menu to delete commitments

--- src/app/(user)/(commitments)/components/actions.tsx
+++ src/app/(user)/(commitments)/components/actions.tsx
@@ -1,7 +1,9 @@
 'use client';
 
 import { Commitment as CommitmentModel, CommitmentStatus } from '@prisma/client';
-import { AiFillPlayCircle, AiOutlineCheckCircle, AiOutlineFieldTime } from 'react-icons/ai';
+import * as Popover from '@radix-ui/react-popover';
+import { AiFillPlayCircle, AiOutlineCheckCircle, AiOutlineDelete, AiOutlineFieldTime } from 'react-icons/ai';
+import { BsThreeDotsVertical } from 'react-icons/bs';
 
 import { styleState } from '@/lib/styling/styleState';
 
@@ -54,6 +56,24 @@ export default function Actions({
     return (
         <div className="flex items-center justify-end gap-3">
             {displayedActions}
+            <div>
+                <Popover.Root>
+                    <Popover.Trigger asChild>
+                        <button type="button" aria-label="More actions">
+                            <BsThreeDotsVertical size={18} />
+                        </button>
+                    </Popover.Trigger>
+                    <Popover.Portal>
+                        <Popover.Content align="start" sideOffset={2} className="rounded border bg-gray-700 border-gray-600 placeholder-gray-400 text-white">
+                            <div>
+                                <button type="button" className="text-red-300 px-4 py-2 hover:bg-gray-600">
+                                    <AiOutlineDelete className="inline-block" /> Delete
+                                </button>
+                            </div>
+                        </Popover.Content>
+                    </Popover.Portal>
+                </Popover.Root>
+            </div>
         </div>
     );
 }
--- src/app/(user)/(commitments)/components/commitment-entry.tsx
+++ src/app/(user)/(commitments)/components/commitment-entry.tsx
@@ -34,10 +34,10 @@ export default function CommitmentEntry({ commitment }: Props) {
             <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                 {thisCommitment.title}
             </th>
-            <td className="px-6 py-4 w-44">
+            <td className="px-6 py-4 w-36">
                 {formatRelativeDate(new Date(thisCommitment.doneBy), now)}
             </td>
-            <td className="px-6 py-4 w-56">
+            <td className="px-6 py-4 w-64">
                 <Actions commitment={thisCommitment}
                     onStart={handleStatusChange(CommitmentStatus.InProgress)}
                     onDone={handleStatusChange(CommitmentStatus.Done)}
