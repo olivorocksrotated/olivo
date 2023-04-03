18043016f7c0895d20f5d63ab2ab6478d2e995cc
Nicolas Baglivo <nicolas.baglivo@gmail.com>
2023-04-03 22:07:21 +0200

    v2

--- src/app/(user)/(commitments)/components/actions/status-btn.tsx
+++ src/app/(user)/(commitments)/components/actions/status-btn.tsx
@@ -1,9 +1,9 @@
 'use client';
 
 import { Commitment as CommitmentModel, CommitmentStatus } from '@prisma/client';
-import { AiFillPlayCircle, AiOutlineCheckCircle, AiOutlineFieldTime } from 'react-icons/ai';
 import { AnimatePresence, motion } from 'framer-motion';
 import { useState } from 'react';
+import { AiFillPlayCircle, AiOutlineCheckCircle, AiOutlineFieldTime } from 'react-icons/ai';
 
 import { styleState } from '@/lib/styling/styleState';
 
@@ -14,64 +14,103 @@ interface Props {
     onDone?: (commitment: Commitment) => void;
 }
 
-function StartButton({ onAnimationComplete }: { onAnimationComplete: () => void}) {
-    const [animationStage, setAnimationStage] = useState(0);
+type TextProps = {
+    text: string;
+    replay: boolean;
+    onAnimationComplete?: () => void,
+    initial?: 'hidden' | 'visible';
+    className: string
+};
 
-    const handleOnClick = () => {
-        setAnimationStage(1);
+function Text({ text, replay, onAnimationComplete = () => null, initial = 'visible', className }: TextProps) {
+    const duration = 0.1;
+    const letters: string[] = Array.from(text);
+    const staggerDirection = initial === 'visible' ? 1 : -1;
+
+    const container = {
+        hidden: {
+            opacity: 0,
+            transition: { delay: 1, staggerChildren: duration, staggerDirection }
+        },
+        visible: {
+            opacity: 1,
+            transition: { staggerChildren: duration, staggerDirection }
+        }
     };
+
+    const child = {
+        visible: {
+            opacity: 1,
+            transition: {
+                type: 'spring',
+                damping: 20,
+                stiffness: 400
+            }
+        },
+        hidden: {
+            opacity: 0,
+            transition: {
+                type: 'spring',
+                damping: 20,
+                stiffness: 400
+            }
+        }
+    };
+
+    return (
+        <motion.h1 layout className={`flex overflow-hidden ${className}`} variants={container}
+            initial={initial}
+            animate={replay ? initial === 'hidden' ? 'visible' : 'hidden' : undefined}
+            onAnimationComplete={onAnimationComplete}
+        >
+            {letters.map((letter, index) => (
+                <motion.span key={index} variants={child}>
+                    {letter === ' ' ? '\u00A0' : letter}
+                </motion.span>
+            ))}
+        </motion.h1>
+    );
+}
+
+function StartButton({
+    onAnimationComplete = () => undefined
+}: { onAnimationComplete?: () => void }) {
+    const [animationStage, setAnimationStage] = useState(0);
 
     const handleOnRest = () => {
         // eslint-disable-next-line no-extra-parens
-        setAnimationStage((prevStage) => (prevStage === 1 ? 2 : prevStage === 2 ? 3 : 0));
+        setAnimationStage((prevStage) => (prevStage === 1 ? 2 : prevStage === 2 ? 3 : 1));
+    };
+
+    const handleOnEnd = () => {
+        onAnimationComplete();
     };
 
     const iconVariants = {
-        start: { x: 0 },
-        end: { x: 'calc(100% - 60px)' }
-    };
-    const textVariants = {
-        done: { opacity: 0, transition: { duration: 0.3 } }
+        start: { width: 80 },
+        mid: { width: 40 },
+        end: { width: 115 }
     };
 
-    const transition = { duration: 1, ease: 'easeInOut' };
+    const transition = { duration: 1.2 };
 
     return (
-        <div className="w-full h-8 relative">
-            <AnimatePresence>
-                <motion.div key="wrapper" layout className="flex absolute right-0 items-center border border-indigo-300 justify-between rounded-lg p-1">
-                    {(animationStage === 0 || animationStage === 1 || animationStage === 2) && (
-                        <motion.div key="play"
-                            initial={animationStage === 0 ? 'start' : undefined}
-                            animate={animationStage === 1 ? 'end' : undefined}
-                            variants={iconVariants}
-                            transition={transition}
-                            onAnimationComplete={animationStage === 1 ? handleOnRest : undefined}
-                        >
-                            <AiFillPlayCircle className="inline-block cursor-pointer"
-                                onClick={animationStage === 0 ? handleOnClick : undefined}
-                            />
-                        </motion.div>
-                    )}
-                    {animationStage === 3 && (
-                        <motion.div key="check"
-                            // className='w-full absolute left-2'
-                            initial="end"
-                            animate={animationStage === 3 ? 'start' : undefined}
-                            variants={iconVariants}
-                            transition={transition}
-                            //   onAnimationComplete={animationStage === 2 ? handleOnRest : undefined}
-                        >
-                            <AiOutlineFieldTime size={25} />
-                        </motion.div>
-                    )}
-                    {(animationStage === 0 || animationStage === 1 || animationStage === 2) && (
-                        <motion.div key="start" onAnimationComplete={animationStage === 2 ? handleOnRest : undefined} initial={{ opacity: 1 }} variants={textVariants} animate={animationStage === 2 ? 'done' : undefined}>Start</motion.div>)}
-                    {animationStage === 3 && (
-                        <motion.div key="ip" initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 0.3 }}>In progress</motion.div>)}
-                </motion.div>
-            </AnimatePresence>
-        </div>
+        <AnimatePresence>
+            <motion.div layout
+                key="container"
+                className="relative h-8 justify-start overflow-hidden items-center flex rounded-full border p-1 pr-3 border-indigo-500"
+                initial="start"
+                animate={animationStage === 1 ? 'mid' : animationStage === 2 ? 'end' : undefined}
+                variants={iconVariants}
+                transition={transition}
+                onAnimationComplete={animationStage === 1 ? handleOnRest : undefined}
+            >
+                {animationStage < 3 && (<motion.div animate={animationStage === 2 ? { opacity: 0, width: 0 } : undefined}><AiFillPlayCircle className="cursor-pointer h-8 mr-2" size={25} onClick={() => setAnimationStage(1)} /></motion.div>)}
+                {animationStage > 1 && (<AiOutlineFieldTime className="h-8 mr-2" size={25} />)}
+                {(animationStage === 0 || animationStage === 1) && (<Text className="absolute right-2" text="Start" replay={animationStage === 1}></Text>)}
+                {animationStage === 2 && (<Text text="In progress" replay={animationStage === 2} onAnimationComplete={handleOnEnd} className="absolute right-2" initial="hidden"></Text>)}
+            </motion.div>
+        </AnimatePresence>
     );
 }
 
@@ -89,13 +128,19 @@ export default function StatusButton({
 
     return (
         !isDone ?
-            <>
+            <div>
                 {!isStarted ?
                     <StartButton onAnimationComplete={handleOnStart}></StartButton> :
-                    <><AiOutlineFieldTime size={25} className="inline-block mr-2" />In progress</>}
-            </> :
-            <div className="rounded-full border p-1 pr-3 border-green-500 text-green-300">
-                <AiOutlineCheckCircle size={25} className="inline-block mr-2" />Done
+                    <div className="rounded-full border p-1 pr-3 border-indigo-500 h-8"><AiOutlineFieldTime size={25} className="inline-block mr-2" />In progress</div>}
+            </div> :
+            <div>
+                <button onClick={handleOnDone}
+                    type="button"
+                    disabled={!isStarted}
+                    className={`rounded-full border p-1 border-green-500 text-green-300 ${styleDisabled}`}
+                >
+                    <AiOutlineCheckCircle size={25} />
+                </button>
             </div>
     );
 }
