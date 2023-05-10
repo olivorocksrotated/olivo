cab9f6229ec85fd4f62b347deb82d3abfd80b44c
Lean <leian1306@gmail.com>
2023-05-10 15:15:21 +0200

    lib: remove unused styling function since it does not work properly with tailwind

--- src/lib/styling/styleState.ts
+++
@@ -1,8 +0,0 @@
-type State = 'disabled' | 'hover' | 'hover:enabled';
-
-export function styleState(state: State, className: string) {
-    return className
-        .split(/\s/)
-        .map((currentClassName) => `${state}:${currentClassName}`)
-        .join(' ');
-}
