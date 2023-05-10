81f1029445b19df950d374e2f9c85d6b8baa799e
Lean <leian1306@gmail.com>
2023-05-10 12:55:19 +0200

    components dialog button: add the effect back

--- src/app/components/dialog-button.tsx
+++ src/app/components/dialog-button.tsx
@@ -1,5 +1,6 @@
 'use client';
 import * as Dialog from '@radix-ui/react-dialog';
+import { AnimatePresence, motion } from 'framer-motion';
 import { MouseEvent, useState } from 'react';
 import { IoCloseOutline } from 'react-icons/io5';
 
@@ -43,28 +44,35 @@ export default function DialogButton({
             <Dialog.Trigger asChild>
                 <div><Button onClick={() => setIsDialogOpen(true)} aria-label={label}>{label}</Button></div>
             </Dialog.Trigger>
-            {isDialogOpen ?
-                <Dialog.Portal forceMount>
-                    <Dialog.Overlay forceMount className="bg-black opacity-60 fixed inset-0" />
-                    <Dialog.Content forceMount
-                        className="fixed top-2/4 left-2/4 -translate-x-1/2 -translate-y-1/2 w-[90vw] max-w-md max-h-[85vh] p-6 rounded bg-gray-900"
-                        onEscapeKeyDown={close}
-                    >
-                        <div className="flex justify-between mb-6">
-                            <Dialog.Title className="font-normal">{label}</Dialog.Title>
-                            <Dialog.Close asChild onClick={close} aria-label="close">
-                                <div><Button><IoCloseOutline /></Button></div>
-                            </Dialog.Close>
-                        </div>
-                        <div>{children}</div>
-                        <div className="text-right">
-                            <Button type="submit" disabled={actionDisabled} onClick={handleOnSubmit} glowing={true} aria-label={label}>
-                                Add
-                            </Button>
-                        </div>
-                    </Dialog.Content>
-                </Dialog.Portal> :
-                null}
+            <AnimatePresence>
+                {isDialogOpen ?
+                    <Dialog.Portal forceMount key="dialog">
+                        <motion.div initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            exit={{ opacity: 0 }}
+                        >
+                            <Dialog.Overlay forceMount className="bg-black opacity-60 fixed inset-0" />
+                            <Dialog.Content forceMount
+                                className="fixed top-2/4 left-2/4 -translate-x-1/2 -translate-y-1/2 w-[90vw] max-w-md max-h-[85vh] p-6 rounded bg-gray-900"
+                                onEscapeKeyDown={close}
+                            >
+                                <div className="flex justify-between mb-6">
+                                    <Dialog.Title className="font-normal">{label}</Dialog.Title>
+                                    <Dialog.Close asChild onClick={close} aria-label="close">
+                                        <div><Button><IoCloseOutline /></Button></div>
+                                    </Dialog.Close>
+                                </div>
+                                <div>{children}</div>
+                                <div className="text-right">
+                                    <Button type="submit" disabled={actionDisabled} onClick={handleOnSubmit} glowing={true} aria-label={label}>
+                                        Add
+                                    </Button>
+                                </div>
+                            </Dialog.Content>
+                        </motion.div>
+                    </Dialog.Portal> :
+                    null}
+            </AnimatePresence>
         </Dialog.Root>
     );
 }
