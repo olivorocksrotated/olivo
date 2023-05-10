bf4824acbc85da455a2dbf3d4f779c8dc184c224
Lean <leian1306@gmail.com>
2023-05-10 12:23:24 +0200

    components dialog button: fix the close behavior and use the new style for the add commitment

--- src/app/(user)/(commitments)/components/add-commitment-btn.tsx
+++ src/app/(user)/(commitments)/components/add-commitment-btn.tsx
@@ -33,24 +33,29 @@ export default function AddCommitmentButton() {
 
     return (
         <DialogButton onClose={() => setCommitment(nullCommitment)} label="Add commitment">
-            <div className="font-bold">Add commitment</div>
-            <div className="flex justify-center items-center gap-2 mt-5">
-                <span className="w-16">I will</span>
-                <input value={commitment.title}
-                    onChange={(event) => setCommitment({ ...commitment, title: event.target.value })}
-                    placeholder="e.g. do this task"
-                    className="w-full h-8 inline-flex items-center justify-centerrounded px-2.5 leading-none outline-none"
-                />
-                <span>by</span>
-                <input type="date"
-                    value={commitment.doneBy}
-                    onChange={(event) => setCommitment({ ...commitment, doneBy: event.target.value })}
-                    placeholder="done by"
-                    className="w-60 h-8 px-2.5"
-                />
-                <Button type="submit" disabled={!commitment.title || !commitment.doneBy} onClick={onSubmit} glowing={true} aria-label="Add commitment">
-                    Add
-                </Button>
+            <div>
+                <div className="flex items-center gap-2 mb-4">
+                    <span className="w-16">I will</span>
+                    <input value={commitment.title}
+                        onChange={(event) => setCommitment({ ...commitment, title: event.target.value })}
+                        placeholder="e.g. do this task"
+                        className="w-full h-8 inline-flex items-center justify-centerrounded px-2.5 leading-none outline-none"
+                    />
+                </div>
+                <div className="flex items-center gap-2 mb-4">
+                    <span>by</span>
+                    <input type="date"
+                        value={commitment.doneBy}
+                        onChange={(event) => setCommitment({ ...commitment, doneBy: event.target.value })}
+                        placeholder="done by"
+                        className="w-60 h-8 px-2.5"
+                    />
+                </div>
+                <div>
+                    <Button type="submit" disabled={!commitment.title || !commitment.doneBy} onClick={onSubmit} glowing={true} aria-label="Add commitment">
+                        Add
+                    </Button>
+                </div>
             </div>
         </DialogButton>
     );
--- src/app/components/dialog-button.tsx
+++ src/app/components/dialog-button.tsx
@@ -1,6 +1,6 @@
 'use client';
 import * as Dialog from '@radix-ui/react-dialog';
-import { useState } from 'react';
+import { MouseEvent, useState } from 'react';
 import { IoCloseOutline } from 'react-icons/io5';
 
 import Button from './button';
@@ -8,24 +8,33 @@ import Button from './button';
 type Properties = {
     children: React.ReactNode;
     label: string;
+    actionDisabled?: boolean;
+    onSubmit: (event: MouseEvent<HTMLButtonElement>) => void
     onClose?: () => void
 };
 
-export default function DialogButton({ children, label, onClose }: Properties) {
+export default function DialogButton({
+    children,
+    label,
+    actionDisabled = false,
+    onSubmit = () => undefined,
+    onClose = () => undefined
+}: Properties) {
     const [isDialogOpen, setIsDialogOpen] = useState(false);
 
-    function onOpenChanged(isOpen: boolean) {
-        if (!isOpen) {
-            onClose!();
-        }
-    }
-
-    async function close() {
+    const close = () => {
         setIsDialogOpen(false);
-    }
+        onClose();
+    };
+
+    const handleOpenChanged = (isOpen: boolean) => {
+        if (!isOpen) {
+            close();
+        }
+    };
 
     return (
-        <Dialog.Root onOpenChange={onOpenChanged}>
+        <Dialog.Root onOpenChange={handleOpenChanged}>
             <Dialog.Trigger asChild>
                 <div><Button onClick={() => setIsDialogOpen(true)} aria-label={label}>{label}</Button></div>
             </Dialog.Trigger>
@@ -36,13 +45,18 @@ export default function DialogButton({ children, label, onClose }: Properties) {
                         className="fixed top-2/4 left-2/4 -translate-x-1/2 -translate-y-1/2 w-[90vw] max-w-md max-h-[85vh] p-6 rounded bg-gray-900"
                         onEscapeKeyDown={close}
                     >
-                        <div className="flex justify-between">
+                        <div className="flex justify-between mb-6">
                             <Dialog.Title className="font-normal">{label}</Dialog.Title>
                             <Dialog.Close asChild onClick={close} aria-label="close">
-                                <Button><IoCloseOutline /></Button>
+                                <div><Button><IoCloseOutline /></Button></div>
                             </Dialog.Close>
                         </div>
                         <div>{children}</div>
+                        <div className="text-right">
+                            <Button type="submit" disabled={actionDisabled} onClick={onSubmit} glowing={true} aria-label={label}>
+                                Add
+                            </Button>
+                        </div>
                     </Dialog.Content>
                 </Dialog.Portal> :
                 null}
