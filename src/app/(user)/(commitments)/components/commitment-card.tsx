26dd5d894e8c062012d91654f3cf8671ee382b9e
Lean <leian1306@gmail.com>
2023-05-10 16:03:52 +0200

    user commitments components card: implement title edition

--- src/app/(user)/(commitments)/components/commitment-card.tsx
+++ src/app/(user)/(commitments)/components/commitment-card.tsx
@@ -1,7 +1,8 @@
 'use client';
 
 import { Commitment as CommitmentModel, CommitmentStatus } from '@prisma/client';
-import { useState } from 'react';
+import clsx from 'clsx';
+import { KeyboardEvent, useState } from 'react';
 
 import { formatRelativeDate } from '@/lib/date/format';
 import { fetchFromApi, ResourcePath } from '@/lib/http/fetch';
@@ -16,7 +17,13 @@ interface Props {
 
 export default function CommitmentCard({ commitment: originalCommitment }: Props) {
     const [commitment, setCommitment] = useState(originalCommitment);
+    const [editTitle, setEditTitle] = useState({ value: commitment.title, isEditing: false });
     const now = new Date();
+
+    const titleStyle = clsx(
+        'truncate',
+        'hover:cursor-pointer hover:underline hover:decoration-dotted'
+    );
 
     const handleStatusChange = async (status: CommitmentStatus) => {
         setCommitment({ ...commitment, status });
@@ -28,13 +35,48 @@ export default function CommitmentCard({ commitment: originalCommitment }: Props
         });
     };
 
+    const handleTitleKeyDown = async (event: KeyboardEvent<HTMLInputElement>) => {
+        if (event.key !== 'Enter') {
+            return;
+        }
+
+        if (commitment.title === editTitle.value) {
+            return setEditTitle((previous) => ({ ...previous, isEditing: false }));
+        }
+
+        const title = editTitle.value;
+        setCommitment({ ...commitment, title });
+        setEditTitle(() => ({ value: title, isEditing: false }));
+        await fetchFromApi({
+            method: HttpMethod.PUT,
+            path: ResourcePath.Commitments,
+            attachToPath: `/${commitment.id}`,
+            body: { title }
+        });
+    };
+
     return (
         <li className="py-3 sm:py-4">
             <div className="flex items-center space-x-4">
                 <div className="min-w-0 flex-1">
-                    <p className="truncate font-medium text-white">
-                        <span className="text-gray-400">I will</span> {commitment.title}
-                    </p>
+                    <div className="flex gap-1 font-medium text-white">
+                        <div>
+                            <span className="text-gray-400">I will</span>{' '}
+                        </div>
+                        <div className="grow">
+                            {!editTitle.isEditing ?
+                                <span className={titleStyle} onClick={() => setEditTitle((previous) => ({ ...previous, isEditing: true }))}>
+                                    {commitment.title}
+                                </span> :
+                                <input type="text"
+                                    autoFocus
+                                    className="w-full bg-slate-700"
+                                    onKeyDown={handleTitleKeyDown}
+                                    value={editTitle.value}
+                                    onChange={(event) => setEditTitle((previous) => ({ ...previous, value: event.target.value }))}
+                                />}
+                        </div>
+                    </div>
                     <p className="truncate text-sm text-gray-400">
                         By {formatRelativeDate(new Date(commitment.doneBy), now)}
                     </p>
