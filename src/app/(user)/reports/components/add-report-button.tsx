e5d18033f63e805bd107e16ac400b2831b5fb383
Lean <leian1306@gmail.com>
2023-05-10 12:42:35 +0200

    user reports components add: use the new dialog button structure

--- src/app/(user)/reports/components/add-report-button.tsx
+++ src/app/(user)/reports/components/add-report-button.tsx
@@ -8,8 +8,6 @@ import DialogButton from '@/app/components/dialog-button';
 import { fetchFromApi, ResourcePath } from '@/lib/http/fetch';
 import { HttpMethod } from '@/lib/http/route';
 
-import Button from '../../../components/button';
-
 function createReportRelationship(emailAddress: string) {
     return fetchFromApi({
         method: HttpMethod.POST,
@@ -53,17 +51,19 @@ export default function AddReportButton() {
     }
 
     return (
-        <DialogButton onClose={reset} label="Add Report">
+        <DialogButton label="Add report"
+            actionDisabled={!email || processing}
+            onClose={reset}
+            onSubmit={onSubmit}
+        >
             <div className="relative">
-                <div className="font-bold">Add Report</div>
-                <div className="flex justify-center items-center gap-2 mt-5">
+                <div className="">
                     <input id="email"
                         value={email}
                         onChange={(event) => setEmail(event.target.value)}
                         placeholder="email"
                         className="w-full h-8 inline-flex items-center justify-center rounded px-2.5 leading-none outline-none"
                     />
-                    <Button type="submit" disabled={!email || processing} onClick={onSubmit} aria-label="Add report">Add</Button>
                 </div>
                 <div className={clsx(
                     'h-12 flex items-center',
