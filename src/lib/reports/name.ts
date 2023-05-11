006c3f1dc175243e5637e8f06048fbd92de3a499
Lean <leian1306@gmail.com>
2023-05-11 17:09:06 +0200

    lib reports name: accept nil values in the functions

--- src/lib/reports/name.ts
+++ src/lib/reports/name.ts
@@ -1,9 +1,17 @@
 const spaceSeparator = /\s/;
 
-export function getNameAcronym(name: string): string {
+export function getNameAcronym(name: string | undefined | null): string {
+    if (!name) {
+        return '';
+    }
+
     return name.split(spaceSeparator).reduce((response, word) => response += word.slice(0, 1), '');
 }
 
-export function getFirstName(name: string): string {
+export function getFirstName(name: string | undefined | null): string {
+    if (!name) {
+        return '';
+    }
+
     return name.split(spaceSeparator)[0];
 }
