183de9cb2f4e81bcf1dff6d471b79289c71dae7b
Lean <leian1306@gmail.com>
2023-05-11 17:09:55 +0200

    user components sidenav: add the user info and the logout button here

--- src/app/(user)/components/sidenav.tsx
+++ src/app/(user)/components/sidenav.tsx
@@ -1,25 +1,31 @@
 'use client';
 
 import * as UiAvatar from '@radix-ui/react-avatar';
-import { useSession } from 'next-auth/react';
+import { signOut, useSession } from 'next-auth/react';
 
-import LogoutButton from './logout-btn';
+import { getFirstName, getNameAcronym } from '@/lib/reports/name';
 
 export default function Sidenav() {
-    const { data: session, status } = useSession();
-
-    console.log('---> user', session, status);
+    const { data: session } = useSession();
+    const nameAcronym = getNameAcronym(session?.user.name ?? '');
+    const firstName = getFirstName(session?.user.name);
 
     return (
         <div>
-            <LogoutButton></LogoutButton>
-            <div>
-                <div>Olivo</div>
+            <div className="flex gap-2">
                 <div>
-                    <UiAvatar.Root className="inline-flex h-20 w-20 select-none items-center justify-center overflow-hidden rounded align-middle">
-                        <UiAvatar.Image src={' '} alt={' '} className="h-full w-full rounded object-cover" />
-                        <UiAvatar.Fallback delayMs={600} className="flex h-full w-full items-center justify-center rounded border border-indigo-400">{' '}</UiAvatar.Fallback>
+                    <UiAvatar.Root className="inline-flex h-7 w-7 select-none items-center justify-center overflow-hidden rounded align-middle">
+                        <UiAvatar.Image src={session?.user.image ?? ''} alt={session?.user.name ?? 'Me'} className="rounded object-cover" />
+                        <UiAvatar.Fallback delayMs={600} className="flex h-full w-full items-center justify-center rounded border border-indigo-400">
+                            {nameAcronym}
+                        </UiAvatar.Fallback>
                     </UiAvatar.Root>
+                </div>
+                <div className="flex flex-col">
+                    <div className="font-normal leading-none">{firstName ?? 'Me'}</div>
+                    <div className="text-sm leading-tight text-slate-300">
+                        <span className="cursor-pointer hover:underline" onClick={() => signOut()}>Logout</span>
+                    </div>
                 </div>
             </div>
         </div>
