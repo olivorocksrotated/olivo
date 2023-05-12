2a4e0cb3d994cf5a758648e682824cee5b91d000
Lean <leian1306@gmail.com>
2023-05-12 21:39:08 +0200

    user commitments components actions delete: simplify the component

--- src/app/(user)/(commitments)/components/actions/delete-btn.tsx
+++ src/app/(user)/(commitments)/components/actions/delete-btn.tsx
@@ -1,55 +1,19 @@
 'use client';
 
-import { useEffect, useRef, useState } from 'react';
 import { AiOutlineDelete } from 'react-icons/ai';
-
-import styles from './delete-btn.module.css';
 
 interface Props {
     onDelete?: () => void;
 }
 
 export default function DeleteButton({ onDelete = () => undefined }: Props) {
-    const [secondsMouseHold, setSecondsMouseHold] = useState(0);
-    const intervalRef = useRef(null as unknown as (null | NodeJS.Timer));
-    const secondsToDelete = 2;
-
-    const isHoldingStyle = secondsMouseHold !== 0 ? styles['wave-up'] : '';
-    if (secondsMouseHold === secondsToDelete) {
-        onDelete();
-    }
-
-    const startHolding = () => {
-        if (intervalRef?.current) {
-            return;
-        }
-
-        setSecondsMouseHold(() => 1);
-        intervalRef.current = setTimeout(() => setSecondsMouseHold((secondsHold) => secondsHold + 1), 1000);
-    };
-
-    const stopHolding = () => {
-        if (intervalRef?.current) {
-            clearInterval(intervalRef.current);
-            intervalRef.current = null;
-            setSecondsMouseHold(() => 0);
-        }
-    };
-
-    useEffect(() => () => stopHolding(), []);
-
     return (
-        <div className={`${styles.circle} cursor-pointer rounded-full border border-red-500 p-1.5 text-red-200`}
-            onMouseDown={startHolding}
-            onTouchStart={startHolding}
-            onMouseUp={stopHolding}
-            onMouseLeave={stopHolding}
-            onTouchEnd={stopHolding}
+        <button type="button"
+            onClick={onDelete}
+            className="text-slate-600"
+            title="Delete commitment"
         >
-            <div className={`${styles.wave} ${isHoldingStyle}`}></div>
-            <div className="relative z-10 flex h-full w-full items-center justify-center">
-                <AiOutlineDelete size={22} />
-            </div>
-        </div>
+            <AiOutlineDelete size={22} />
+        </button>
     );
 }
