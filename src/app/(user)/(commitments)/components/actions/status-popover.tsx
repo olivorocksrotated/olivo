5c6dd7efd0ffc61d3d677028073ea1e07b9568bc
Lean <leian1306@gmail.com>
2023-05-12 21:39:57 +0200

    user commitments components actions: add a popover to handle status changes

---
+++ src/app/(user)/(commitments)/components/actions/status-popover.tsx
@@ -0,0 +1,52 @@
+import { Commitment, CommitmentStatus } from '@prisma/client';
+import * as Popover from '@radix-ui/react-popover';
+
+import DoneStatusMarker from '../status-marker/done';
+import InProgressStatusMarker from '../status-marker/in-progress';
+import NotStartedStatusMarker from '../status-marker/not-started';
+
+interface Props {
+    commitment: Pick<Commitment, 'status'>;
+    onStatusChange: (status: CommitmentStatus) => void
+}
+
+export default function StatusPopover({ commitment, onStatusChange }: Props) {
+    return (
+        <Popover.Root>
+            <Popover.Trigger asChild>
+                <button type="button" aria-label="Change status">
+                    <div>
+                        {
+                            commitment.status === CommitmentStatus.NotStartedYet ? <NotStartedStatusMarker /> :
+                            commitment.status === CommitmentStatus.InProgress ? <InProgressStatusMarker /> :
+                            commitment.status === CommitmentStatus.Done ? <DoneStatusMarker /> : null
+                        }
+                    </div>
+                </button>
+            </Popover.Trigger>
+            <Popover.Portal>
+                <Popover.Content align="start">
+                    <div className="w-52 rounded bg-gray-700 p-2 text-sm shadow-sm will-change-transform">
+                        <div onClick={() => onStatusChange(CommitmentStatus.NotStartedYet)}
+                            className="mb-2 flex cursor-pointer items-center gap-x-2 rounded px-2 py-1 hover:bg-gray-600"
+                        >
+                            <NotStartedStatusMarker />
+                            <span>Not started</span>
+                        </div>
+                        <div onClick={() => onStatusChange(CommitmentStatus.InProgress)}
+                            className="mb-2 flex cursor-pointer items-center gap-x-2 rounded px-2 py-1 hover:bg-gray-600"
+                        >
+                            <InProgressStatusMarker /><span>In progress</span>
+                        </div>
+                        <div onClick={() => onStatusChange(CommitmentStatus.Done)}
+                            className="flex cursor-pointer items-center gap-x-2 rounded px-2 py-1 hover:bg-gray-600"
+                        >
+                            <DoneStatusMarker /><span>Done</span>
+                        </div>
+                    </div>
+                    <Popover.Arrow className="fill-gray-700" />
+                </Popover.Content>
+            </Popover.Portal>
+        </Popover.Root>
+    );
+}
