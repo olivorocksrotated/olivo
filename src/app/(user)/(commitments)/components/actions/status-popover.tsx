d44d04212bfbc963890b57f6c2c4282e2044c430
Lean <leian1306@gmail.com>
2023-05-12 21:49:06 +0200

    user commitments components actions popover: close the popover after clicking

--- src/app/(user)/(commitments)/components/actions/status-popover.tsx
+++ src/app/(user)/(commitments)/components/actions/status-popover.tsx
@@ -1,6 +1,10 @@
+'use client';
+
 import { Commitment, CommitmentStatus } from '@prisma/client';
 import * as Popover from '@radix-ui/react-popover';
+import { useState } from 'react';
 
+import AbandonedStatusMarker from '../status-marker/abandoned';
 import DoneStatusMarker from '../status-marker/done';
 import InProgressStatusMarker from '../status-marker/in-progress';
 import NotStartedStatusMarker from '../status-marker/not-started';
@@ -11,15 +15,23 @@ interface Props {
 }
 
 export default function StatusPopover({ commitment, onStatusChange }: Props) {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleStatusChange = (status: CommitmentStatus) => {
+        onStatusChange(status);
+        setIsOpen(false);
+    };
+
     return (
-        <Popover.Root>
+        <Popover.Root onOpenChange={setIsOpen} open={isOpen}>
             <Popover.Trigger asChild>
                 <button type="button" aria-label="Change status">
                     <div>
                         {
                             commitment.status === CommitmentStatus.NotStartedYet ? <NotStartedStatusMarker /> :
                             commitment.status === CommitmentStatus.InProgress ? <InProgressStatusMarker /> :
-                            commitment.status === CommitmentStatus.Done ? <DoneStatusMarker /> : null
+                            commitment.status === CommitmentStatus.Done ? <DoneStatusMarker /> :
+                            commitment.status === CommitmentStatus.Abandoned ? <AbandonedStatusMarker /> : null
                         }
                     </div>
                 </button>
@@ -27,18 +39,18 @@ export default function StatusPopover({ commitment, onStatusChange }: Props) {
             <Popover.Portal>
                 <Popover.Content align="start">
                     <div className="w-52 rounded bg-gray-700 p-2 text-sm shadow-sm will-change-transform">
-                        <div onClick={() => onStatusChange(CommitmentStatus.NotStartedYet)}
+                        <div onClick={() => handleStatusChange(CommitmentStatus.NotStartedYet)}
                             className="mb-2 flex cursor-pointer items-center gap-x-2 rounded px-2 py-1 hover:bg-gray-600"
                         >
                             <NotStartedStatusMarker />
                             <span>Not started</span>
                         </div>
-                        <div onClick={() => onStatusChange(CommitmentStatus.InProgress)}
+                        <div onClick={() => handleStatusChange(CommitmentStatus.InProgress)}
                             className="mb-2 flex cursor-pointer items-center gap-x-2 rounded px-2 py-1 hover:bg-gray-600"
                         >
                             <InProgressStatusMarker /><span>In progress</span>
                         </div>
-                        <div onClick={() => onStatusChange(CommitmentStatus.Done)}
+                        <div onClick={() => handleStatusChange(CommitmentStatus.Done)}
                             className="flex cursor-pointer items-center gap-x-2 rounded px-2 py-1 hover:bg-gray-600"
                         >
                             <DoneStatusMarker /><span>Done</span>
