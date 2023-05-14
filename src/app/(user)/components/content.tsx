d81edca83c3a2e6febca363d36831659a30aaca4
Nicolas Baglivo <nicolas.baglivo@gmail.com>
2023-05-14 23:12:59 +0200

    src app (user) components content: add animation between transitions on links

--- src/app/(user)/components/content.tsx
+++ src/app/(user)/components/content.tsx
@@ -4,63 +4,113 @@ import clsx from 'clsx';
 import { AnimatePresence, motion } from 'framer-motion';
 import Image from 'next/image';
 import Link from 'next/link';
+import { usePathname, useSearchParams, useSelectedLayoutSegment } from 'next/navigation';
 import { signOut, useSession } from 'next-auth/react';
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import { AiFillHome } from 'react-icons/ai';
 import { BsPeopleFill } from 'react-icons/bs';
-import { FaTasks } from 'react-icons/fa';
+import { FaSignOutAlt, FaTasks, FaWrench } from 'react-icons/fa';
 import { TbMoodCheck } from 'react-icons/tb';
 
 import { getFirstName, getNameAcronym } from '@/lib/name/name';
 
-function UserMenu({ children, isOpen, id }: { children: React.ReactNode; isOpen: boolean; id: string }) {
+function UserMenu({ children }: { children: React.ReactNode; }) {
+    return (
+        <div className="mx-1 my-4 flex items-center justify-center divide-y divide-gray-600">
+            <div className="rounded-lg bg-indigo-950/90 px-5 py-1">
+                {children}
+            </div>
+        </div>
+    );
+}
+
+function HoverMark() {
+    return (
+        <motion.div layoutId="hovered" className="absolute top-0 h-full w-2">
+            <div className="h-full w-full rounded bg-purple-900" />
+        </motion.div>
+    );
+}
+
+function SelectedBackground() {
+    return (
+        <motion.div layoutId="selected" className="absolute top-0 h-full w-full">
+            <div className="h-full w-full rounded bg-indigo-950" />
+        </motion.div>
+    );
+}
+
+function NavigationLinks() {
+    const selected = useSelectedLayoutSegment();
+    const [hovered, setHovered] = useState<number>();
+
+    const linkClasses = 'relative flex items-center rounded-lg p-2 pl-4 text-sm font-thin text-white focus:font-semibold';
+    const iconClasses = 'mr-3 text-gray-400';
+    const selectedHoverIndex = {
+        home: 1,
+        commitments: 2,
+        network: 3,
+        moods: 4
+    }[selected || 'home'];
+
     return (
         <AnimatePresence mode="wait">
-            {isOpen ? (
-                <motion.div initial={{
-                    height: 0,
-                    opacity: 0
-                }}
-                animate={{
-                    height: 'auto',
-                    opacity: 1,
-                    transition: {
-                        height: { duration: 0.2 },
-                        opacity: {
-                            duration: 0.25,
-                            delay: 0.05
-                        }
-                    }
-                }}
-                exit={{
-                    height: 0,
-                    opacity: 0,
-                    transition: {
-                        height: { duration: 0.2 },
-                        opacity: { duration: 0.25 }
-                    }
-                }}
-                key="dropdown-user"
-                className="divide-y divide-gray-600 rounded bg-gray-700 text-base shadow"
-                id={id}
-                >
-                    {children}
-                </motion.div>
-            ) : null}
+            <ul onMouseLeave={() => setHovered(selectedHoverIndex)} className="mt-4 space-y-2 border-t border-gray-700 pt-4">
+                <li onMouseEnter={() => setHovered(1)} className="relative">
+                    {selected === null ? <SelectedBackground /> : null}
+                    {hovered === 1 ? <HoverMark /> : null}
+                    <Link href="/" className={linkClasses}>
+                        <AiFillHome size={18} className={iconClasses} />
+                        <span>Home</span>
+                    </Link>
+                </li>
+                <li onMouseEnter={() => setHovered(2)} className="relative">
+                    {selected === 'commitments' ? <SelectedBackground /> : null}
+                    {hovered === 2 ? <HoverMark /> : null}
+                    <Link href="/commitments" className={linkClasses}>
+                        <FaTasks size={18} className={iconClasses} />
+                        <span>Commitments</span>
+                    </Link>
+                </li>
+                <li onMouseEnter={() => setHovered(3)} className="relative">
+                    {selected === 'network' ? <SelectedBackground /> : null}
+                    {hovered === 3 ? <HoverMark /> : null}
+                    <Link href="/network" className={linkClasses}>
+                        <BsPeopleFill size={18} className={iconClasses} />
+                        <span>Your Network</span>
+                    </Link>
+                </li>
+                <li onMouseEnter={() => setHovered(4)} className="relative">
+                    {selected === 'moods' ? <SelectedBackground /> : null}
+                    {hovered === 4 ? <HoverMark /> : null}
+                    <Link href="/moods" className={linkClasses}>
+                        <TbMoodCheck size={18} className={iconClasses} />
+                        <span>Your Moods</span>
+                    </Link>
+                </li>
+            </ul>
         </AnimatePresence>
     );
+}
+
+function useRouterEvents(callback: () => void) {
+    const pathname = usePathname();
+    const searchParams = useSearchParams();
+
+    return useCallback(() => {
+        callback();
+    }, [pathname, searchParams, callback]);
 }
 
 export default function Content({ children }: { children: React.ReactNode }) {
     const { data: session } = useSession();
     const [isMobileOpen, setIsMobileOpen] = useState(false);
-    const [isUserMenuOpen, setIsUserMenuOpen] = useState(false);
+    const closeMobileMenu = () => setIsMobileOpen(false);
+    useRouterEvents(closeMobileMenu);
 
     const nameAcronym = getNameAcronym(session?.user.name);
     const firstName = getFirstName(session?.user.name);
     const asideMobileStyle = clsx({ 'translate-x-0': isMobileOpen });
-
-    const closeMobileMenu = () => setIsMobileOpen(false);
 
     return (
         <div>
@@ -81,59 +131,32 @@ export default function Content({ children }: { children: React.ReactNode }) {
                 className={`fixed left-0 top-0 z-40 h-screen w-56 -translate-x-full transition-transform sm:translate-x-0 ${asideMobileStyle}`}
                 aria-label="Sidebar"
             >
-                <div className="h-full overflow-y-auto bg-gray-900 px-3 py-4">
-                    <ul className="space-y-2 font-medium">
-                        <li>
-                            <div className="flex gap-2">
-                                <div>
-                                    <button type="button"
-                                        onClick={() => setIsUserMenuOpen((previous) => !previous)}
-                                        className="mb-4 flex rounded-full bg-gray-800 text-sm focus:ring-4 focus:ring-gray-600"
-                                        aria-expanded="false"
-                                        data-dropdown-toggle="dropdown-user"
-                                    >
-                                        <span className="sr-only">Open user menu</span>
-                                        <Image className="rounded-full" width={32} height={32} src={session?.user.image ?? ''} alt={nameAcronym} />
-                                    </button>
-                                </div>
-                                <div className="" role="none">
-                                    <p className="text-sm font-light text-white" role="none">{firstName ?? 'Me'}</p>
-                                    <p className="truncate text-xs font-light text-gray-300" role="none">{session?.user.email}</p>
-                                </div>
+                <div className="h-full overflow-y-auto px-3 py-4" style={{ background: 'rgb(22, 24, 29)' }}>
+                    <div className="space-y-2 font-medium">
+                        <div className="flex gap-2">
+                            <div>
+                                <span className="sr-only">Open user menu</span>
+                                <Image className="rounded-full" width={32} height={32} src={session?.user.image ?? ''} alt={nameAcronym} />
                             </div>
-                            <UserMenu id="dropdown-user" isOpen={isUserMenuOpen}>
-                                <ul className="list-none py-1" role="none">
-                                    <li>
-                                        <Link href="#" onClick={() => signOut()} className="block px-4 py-2 text-sm font-light text-gray-300 hover:bg-gray-600 hover:text-white" role="menuitem">Logout</Link>
-                                    </li>
-                                </ul>
-                            </UserMenu>
-                        </li>
-                        <li className="mt-4 space-y-2 border-t border-gray-700 pt-4">
-                            <Link href="/" onClick={closeMobileMenu} className="flex items-center rounded-lg p-2 text-sm font-thin text-white hover:bg-gray-700">
-                                <AiFillHome size={18} className="mr-3 text-gray-400" />
-                                <span>Home</span>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href="/commitments" onClick={closeMobileMenu} className="flex items-center rounded-lg p-2 text-sm font-thin text-white hover:bg-gray-700">
-                                <FaTasks size={18} className="mr-3 text-gray-400" />
-                                <span>Commitments</span>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href="/network" onClick={closeMobileMenu} className="flex items-center rounded-lg p-2 text-sm font-thin text-white hover:bg-gray-700">
-                                <BsPeopleFill size={18} className="mr-3 text-gray-400" />
-                                <span>Your network</span>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href="/moods" onClick={closeMobileMenu} className="flex items-center rounded-lg p-2 text-sm font-thin text-white hover:bg-gray-700">
-                                <TbMoodCheck size={18} className="mr-3 text-gray-400" />
-                                <span>Your mood</span>
-                            </Link>
-                        </li>
-                    </ul>
+                            <div role="none">
+                                <p className="text-sm font-light text-white" role="none">{firstName ?? 'Me'}</p>
+                                <p className="truncate text-xs font-light text-gray-300" role="none">{session?.user.email}</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <UserMenu>
+                        <ul className="grid list-none grid-cols-3 gap-2 py-1" role="none">
+                            <li className="cursor-pointer rounded-xl border-2 border-indigo-700 px-3 py-1 text-sm text-white">
+                                <FaSignOutAlt onClick={() => signOut()} className="h-3 w-3" role="menuitem" />
+                            </li>
+                            <li className="cursor-pointer rounded-xl border-2 border-indigo-700 px-3 py-1 text-sm text-white">
+                                <FaWrench onClick={() => signOut()} className="h-3 w-3" role="menuitem" />
+                            </li>
+                        </ul>
+                    </UserMenu>
+
+                    <NavigationLinks ></NavigationLinks>
                 </div>
             </aside>
 
