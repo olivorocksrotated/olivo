b4f29fbc2373b097d6f2d219eb532b42bc052abc
Lean <leian1306@gmail.com>
2023-05-15 18:03:56 +0200

    user mood selector: implement a first version connected to server actions

--- src/app/(user)/(mood)/mood-selector.tsx
+++ src/app/(user)/(mood)/mood-selector.tsx
@@ -1,93 +1,110 @@
 'use client';
 
-import { MoodStatus } from '@prisma/client';
+import { Mood, MoodStatus } from '@prisma/client';
 import clsx from 'clsx';
+import { isToday } from 'date-fns';
 import { motion } from 'framer-motion';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useZact } from 'zact/client';
 
-interface MoodChoice {
-    icon: string;
-    name: string;
+import { createMoodAction } from '@/lib/moods/create';
+import { updateMoodAction } from '@/lib/moods/update';
+
+interface Props {
+    latestMood: Pick<Mood, 'id' | 'status' | 'comment' | 'createdAt'> | null;
 }
 
-export default function MoodSelector() {
-    const nullMood: MoodChoice = { icon: '', name: '' };
-    const moods: MoodChoice[] = [
-        { icon: '💩', name: MoodStatus.Bad },
-        { icon: '😓', name: MoodStatus.Okayish },
-        { icon: '🆗', name: MoodStatus.Average },
-        { icon: '🙂', name: MoodStatus.Good },
-        { icon: '💚', name: MoodStatus.Excellent }
-    ];
+interface MoodOption {
+    icon: string;
+    name: MoodStatus;
+}
 
-    const [currentMoodChoice, setCurrentMoodChoice] = useState(nullMood);
-    const [comment, setComment] = useState('');
+const moodOptions: MoodOption[] = [
+    { icon: '💩', name: MoodStatus.Bad },
+    { icon: '😓', name: MoodStatus.Okayish },
+    { icon: '🆗', name: MoodStatus.Average },
+    { icon: '🙂', name: MoodStatus.Good },
+    { icon: '💚', name: MoodStatus.Excellent }
+];
+const nullMoodOption: MoodOption = { icon: '', name: '' as MoodStatus };
+const nullState = { option: nullMoodOption, comment: '' };
 
-    const moodStyle = clsx(
-        'flex w-20 cursor-pointer flex-col items-center rounded bg-slate-700 p-2 transition',
-        'hover:bg-slate-600'
-    );
+export default function MoodSelector({ latestMood }: Props) {
+    const [moodChoice, setMoodChoice] = useState(nullState);
+    useEffect(() => setMoodChoice(() => {
+        const isMoodCheckAlreadyDoneToday = !!latestMood && isToday(latestMood.createdAt);
 
-    const parentHeight = { initial: '150px', expanded: '250px' };
+        return !isMoodCheckAlreadyDoneToday ? nullState : {
+            option: moodOptions.find(({ name }) => name === latestMood!.status) ?? nullMoodOption,
+            comment: latestMood.comment ?? ''
+        };
+    }), [latestMood]);
 
-    const handleMoodClick = (mood: MoodChoice) => {
-        setCurrentMoodChoice(mood);
+    const { mutate: createMood } = useZact(createMoodAction);
+    const { mutate: updateMood } = useZact(updateMoodAction);
+
+    const handleMoodClick = async (mood: MoodOption) => {
+        setMoodChoice((previous) => ({ ...previous, option: mood }));
+        const action = !latestMood?.id ?
+            createMood({ status: mood.name }) :
+            updateMood({ id: latestMood!.id, status: mood.name });
+
+        await action;
     };
 
-    const handleSaveComment = () => {
-        setCurrentMoodChoice(nullMood);
-        setComment('');
+    const handleSaveComment = async () => {
+        await updateMood({ id: latestMood!.id, comment: moodChoice.comment });
     };
+
+    const parentContainerHeight = { initial: !latestMood?.id ? '150px' : '250px', expanded: '250px' };
 
     return (
         <motion.div className="rounded border border-indigo-700 bg-gray-800 p-4 sm:w-fit"
-            initial={{ height: parentHeight.initial }}
-            animate={{ height: currentMoodChoice.name ? parentHeight.expanded : parentHeight.initial }}
+            initial={{ height: parentContainerHeight.initial }}
+            animate={{ height: moodChoice.option?.name ? parentContainerHeight.expanded : parentContainerHeight.initial }}
         >
-            <div className="mb-4 text-lg text-white">
-                How are you feeling today?
-            </div>
+            <div className="mb-4 text-lg text-white">How are you feeling today?</div>
             <div className="mb-4 flex gap-2">
-                {moods.map((mood) => (
+                {moodOptions.map((mood) => (
                     <div key={mood.name}
                         onClick={() => handleMoodClick(mood)}
-                        className={`${moodStyle} ${clsx({ 'bg-slate-500 hover:bg-slate-500': mood.name === currentMoodChoice.name })}`}
+                        className={clsx({
+                            'flex w-20 cursor-pointer flex-col items-center rounded p-2 transition': true,
+                            'bg-slate-700 hover:bg-slate-600': mood.name !== moodChoice.option?.name,
+                            'bg-slate-500 hover:bg-slate-500': mood.name === moodChoice.option?.name
+                        })}
                     >
                         <div className="text-2xl">{mood.icon}</div>
                         <div className="hidden sm:block">{mood.name}</div>
                     </div>
                 ))}
             </div>
-            {currentMoodChoice.name ?
-                <motion.div className="border-t border-gray-700 pt-3"
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    exit={{ opacity: 0 }}
-                    transition={{ duration: 1 }}
-                >
+            {moodChoice.option?.name ?
+                <div className="border-t border-gray-700 pt-3">
                     <div className="mb-2">Would you like to share a bit more?</div>
                     <form>
                         <div className="flex gap-2">
                             <div className="w-full">
                                 <input type="text"
                                     autoFocus
-                                    value={comment}
-                                    onChange={(event) => setComment(event.target.value)}
-                                    placeholder={`What is making you feel ${currentMoodChoice.name.toLowerCase()}?`}
+                                    value={moodChoice.comment}
+                                    onChange={(event) => setMoodChoice((previous) => ({ ...previous, comment: event.target.value }))}
+                                    placeholder={`What is making you feel ${moodChoice.option.name.toLowerCase()}?`}
                                     className="w-full p-2"
                                 />
                             </div>
                             <div>
                                 <button type="submit"
-                                    disabled={!comment}
-                                    className="rounded border border-slate-400 px-3 py-2 hover:enabled:border-slate-300 disabled:opacity-50" onClick={handleSaveComment}
+                                    disabled={!moodChoice.comment || !latestMood?.id}
+                                    onClick={handleSaveComment}
+                                    className="rounded border border-slate-400 px-3 py-2 hover:enabled:border-slate-300 disabled:opacity-50"
                                 >
-                                        Save
+                                    Save
                                 </button>
                             </div>
                         </div>
                     </form>
-                </motion.div> : null}
+                </div> : null}
         </motion.div>
     );
 }
--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -1,5 +1,6 @@
 import { getServerSession } from '@/lib/auth/session';
 import { getCommitmentsByUser } from '@/lib/commitments/get';
+import { getLatestMood } from '@/lib/moods/get';
 import { getFirstName } from '@/lib/name/name';
 
 import PageTitle from '../components/page-title';
@@ -11,12 +12,13 @@ export default async function Home() {
     const { user } = await getServerSession();
     const firstName = getFirstName(user.name);
 
+    const latestMood = await getLatestMood(user.id);
     const commitments = await getCommitmentsByUser(user.id, { today: true });
 
     return (
         <main>
             <PageTitle text={`👋 Hey, ${firstName}`} />
-            <div className="mb-10"><MoodSelector /></div>
+            <div className="mb-10"><MoodSelector latestMood={latestMood} /></div>
             <div className="mb-10">{await NextMeetingCard()}</div>
             <div>{await CommitmentsSection({ commitments, title: 'Commitments for today' })}</div>
         </main>
