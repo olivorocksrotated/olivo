7d33c3c2988fece697cd82e06b23f8d6e78d8cb4
Lean <leian1306@gmail.com>
2023-05-16 13:09:09 +0200

    user moods components list: display the most relevant information here

--- src/app/(user)/moods/components/moods-list.tsx
+++ src/app/(user)/moods/components/moods-list.tsx
@@ -1,16 +1,29 @@
 import { Mood } from '@prisma/client';
 
+import { formatRelativeDate } from '@/lib/date/format';
+
+import { moodOptions } from '../constants';
+
 interface Props {
     moods: Pick<Mood, 'id' | 'comment' | 'status' | 'createdAt'>[];
 }
 
+
 export default function MoodsList({ moods }: Props) {
+    const now = new Date();
+
     return (
-        <div>
+        <div className="w-fit">
             <ul role="list" className="divide-y divide-gray-700">
                 {moods.map((mood) => (
                     <li key={mood.id} className="py-3 sm:py-4">
-                        {mood.status}
+                        <div className="text-lg font-normal">
+                            {moodOptions[mood.status].icon} {formatRelativeDate(mood.createdAt, now)} you were feeling <strong>{mood.status.toLowerCase()}</strong>
+                        </div>
+                        {mood.comment ? (
+                            <div>
+                                <span className="text-gray-400">Because...</span> <span>{mood.comment}</span>
+                            </div>) : null}
                     </li>
                 ))}
             </ul>
