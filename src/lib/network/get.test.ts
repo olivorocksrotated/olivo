6fd0e4378d8ffa0291b03a0bcabc725acb6fd067
Nicolas Baglivo <nicolas.baglivo@gmail.com>
2023-05-18 19:13:33 +0200

    src lib network get: fix tests

--- src/lib/network/get.test.ts
+++ src/lib/network/get.test.ts
@@ -8,23 +8,16 @@ vi.mock('../prisma', async () => ({
     default: await generatePrismock()
 }));
 
-vi.mock('next/headers', async () => ({
-    ...await vi.importActual('next/headers') as any,
-    cookies: vi.fn().mockReturnValue({
-        getAll: () => [
-            // fake user cookies
-            { name: 'next-auth.session-token',
-                value: 'eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2R0NNIn0.._H9VIrWXurNPYcG-.aZDn0Gbp3KTdrb4gEiT1dN9VN5IpAtn442TtaiSy5k9hrA3Sg4Zt4nW74UPJJWikJAhcoGOnD7O9J5lFZLZ6S3kjefpb2n5j38Nbr-18XOId4kMKk2zz21pRHNFIJr9AU0WzKESC5Wi5V6R3haOohNAEJqVkoOIhhuRm70cahXkTgiCz57jA.9Z7wPEbYaVaYa56CgI_8cg' }
-        ]
-    }),
-    headers: vi.fn().mockReturnValue([])
+vi.mock('../auth/session', async () => ({
+    getServerSession: vi.fn().mockResolvedValue({ user: { id: '1' } })
 }));
 
 describe('lib network', () => {
     describe('get', () => {
         describe('getNetwork', () => {
+            const requesterId = '1';
+
             it('should return the network of the user', async () => {
-                const requesterId = '1';
                 const expectedUsers = [
                     { id: '2', image: 'image2', name: 'name2' },
                     { id: '3', image: 'image3', name: 'name3' }
@@ -40,7 +33,6 @@ describe('lib network', () => {
             });
 
             it('should return default values if the acceptor user does not have name and image', async () => {
-                const requesterId = '1';
                 const expectedUser = { id: '4', image: null, name: null };
                 prisma.user.create({ data: expectedUser });
                 prisma.networkConnection.create({ data: { requesterId, acceptorId: expectedUser.id } });
