668d83582d351e7407787d601209d8d4b4d2a6f2
Nicolas Baglivo <nicolas.baglivo@gmail.com>
2023-05-18 17:00:11 +0200

    app user network: keep using connection and network concepts. Remove title

--- src/app/(user)/network/page.tsx
+++ src/app/(user)/network/page.tsx
@@ -2,19 +2,18 @@ import PageTitle from '@/app/components/page-title';
 import { getServerSession } from '@/lib/auth/session';
 import { getReportsByManager } from '@/lib/reports/get';
 
-import AddReportButton from './components/connect-button';
+import ConnectButton from './components/connect-button';
 import Connection from './components/connection';
 
-export default async function Reports() {
+export default async function Network() {
     const { user } = await getServerSession();
-    const reports = await getReportsByManager(user.id);
+    const connections = await getReportsByManager(user.id);
 
-    const hasReports = reports.length !== 0;
-    const reportsList = (
+    const hasNetwork = connections.length !== 0;
+    const network = (
         <div className="max-w-fit rounded-lg bg-indigo-900 p-4">
-            <div className="mb-4 p-3 text-lg font-normal">Upcoming 1:1s</div>
             <div className="divide-y divide-indigo-700">
-                {reports.map((connection) => (
+                {connections.map((connection) => (
                     <div key={connection.id}>
                         <Connection connection={connection} />
                     </div>
@@ -22,7 +21,8 @@ export default async function Reports() {
             </div>
         </div>
     );
-    const noReports = (
+
+    const emptyNetworkMessage = (
         <div>The network is empty for now, start connecting to people!</div>
     );
 
@@ -30,9 +30,9 @@ export default async function Reports() {
         <main>
             <PageTitle text="Network" />
             <div className="py-10">
-                <AddReportButton></AddReportButton>
+                <ConnectButton></ConnectButton>
             </div>
-            {hasReports ? reportsList : noReports}
+            {hasNetwork ? network : emptyNetworkMessage}
         </main>
     );
 }
