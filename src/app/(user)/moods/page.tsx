0ae48bd3618aa0185fc9ead4caa85a50648fc2ed
Lean <leian1306@gmail.com>
2023-05-19 14:05:16 +0200

    user mooods components matrix: move the component to its own folder

--- src/app/(user)/moods/page.tsx
+++ src/app/(user)/moods/page.tsx
@@ -2,22 +2,18 @@ import PageTitle from '@/app/components/page-title';
 import { getServerSession } from '@/lib/auth/session';
 import { getMoods } from '@/lib/moods/get';
 
-import MoodMatrix from './components/mood-matrix';
+import MoodMatrix from './components/matrix/mood-matrix';
 import MoodTrend from './components/mood-trends';
 
 export default async function Moods() {
     const { user } = await getServerSession();
-    const moodsForList = await getMoods({
-        userId: user.id,
-        filters: { created: 'this month' }
-    });
-    const moodsForTrend = await getMoods({
+    const moods = await getMoods({
         userId: user.id,
         filters: { created: 'this month' },
         order: 'asc'
     });
 
-    const hasMoods = moodsForList.length !== 0;
+    const hasMoods = moods.length !== 0;
     const noMoods = (
         <div>You do not have any moods yet</div>
     );
@@ -29,8 +25,8 @@ export default async function Moods() {
             <div>
                 <div className="mb-8 text-lg">How you have been feeling this month</div>
                 <div className="flex flex-col-reverse gap-12 sm:flex-row sm:align-top">
-                    <div className="w-fit"><MoodMatrix moods={moodsForList} /></div>
-                    <div className="max-w-3xl grow"><MoodTrend moods={moodsForTrend} /></div>
+                    <div className="w-fit"><MoodMatrix moods={moods} /></div>
+                    <div className="max-w-3xl grow"><MoodTrend moods={moods} /></div>
                 </div>
             </div>}
         </main>
