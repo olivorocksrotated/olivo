12989e091d1648123a717d65baa13b51661da7e4
Lean <leian1306@gmail.com>
2023-05-19 15:10:02 +0200

    user moods components matrix entry: display a tooltip on hover when there is a status set

--- src/app/(user)/moods/components/matrix/mood-entry.tsx
+++ src/app/(user)/moods/components/matrix/mood-entry.tsx
@@ -1,6 +1,8 @@
 import * as Tooltip from '@radix-ui/react-tooltip';
 import clsx from 'clsx';
 
+import { formatRelativeDate } from '@/lib/date/format';
+
 import { colorScale, MatrixMood } from './constants';
 
 type MoodEntryModel = MatrixMood & { selected: boolean | null };
@@ -11,29 +13,36 @@ interface Props {
 }
 
 export default function MoodEntry({ mood, onEntrySelected }: Props) {
-    return (
-        <Tooltip.Provider key={mood.id} delayDuration={400} skipDelayDuration={1000}>
+    const now = new Date();
+    const hasStatus = mood.status !== null;
+    const entry = (
+        <div onClick={() => onEntrySelected(mood)}
+            className={clsx({
+                [colorScale[mood.status!]]: mood.status !== null,
+                'rounded cursor-pointer': true,
+                'bg-green-400': mood.status !== null || mood.selected,
+                'bg-neutral-700 !opacity-100': mood.status === null || mood.selected === false
+            })}
+        >
+        </div>
+    );
+    const entryWithStatus = (
+        <Tooltip.Provider delayDuration={400} skipDelayDuration={1000}>
             <Tooltip.Root>
-                <Tooltip.Trigger asChild>
-                    <div onClick={() => onEntrySelected(mood)}
-                        className={clsx({
-                            [colorScale[mood.status!]]: mood.status !== null,
-                            rounded: true,
-                            'bg-green-400 cursor-pointer': mood.status !== null || mood.selected,
-                            'bg-neutral-700 !opacity-100': mood.status === null || mood.selected === false
-                        })}
-                    >
-                    </div>
-                </Tooltip.Trigger>
+                <Tooltip.Trigger asChild>{entry}</Tooltip.Trigger>
                 <Tooltip.Portal>
-                    <Tooltip.Content sideOffset={5}
-                        className="max-w-xs select-none rounded bg-slate-600 p-3 text-sm text-white shadow-sm"
-                    >
-                        {mood.comment}
+                    <Tooltip.Content sideOffset={5} className="max-w-xs select-none rounded bg-slate-600 p-3 text-sm text-white shadow-sm">
+                        <div className="text-sm">
+                            <span>{mood.status}</span>{' '}
+                            <span className="text-slate-400"># {formatRelativeDate(mood.createdAt, now)}</span>
+                        </div>
+                        <div className="font-normal">{mood.comment ?? 'No comment here'}</div>
                         <Tooltip.Arrow className="fill-slate-600" />
                     </Tooltip.Content>
                 </Tooltip.Portal>
             </Tooltip.Root>
         </Tooltip.Provider>
     );
+
+    return !hasStatus ? entry : entryWithStatus;
 }
