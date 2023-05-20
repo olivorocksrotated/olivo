0eeaf1a30c5db5fd2b36f472cad0fb7e7f8bc01a
Lean <leian1306@gmail.com>
2023-05-20 23:12:59 +0200

    user commitments components list: implement filtering

--- src/app/(user)/commitments/components/list/commitments-list.tsx
+++ src/app/(user)/commitments/components/list/commitments-list.tsx
@@ -2,21 +2,46 @@
 
 import { useAutoAnimate } from '@formkit/auto-animate/react';
 import { Commitment } from '@prisma/client';
+import { useEffect, useState } from 'react';
+
+import { isNotDone, isPast } from '@/lib/commitments/filter';
+import { todayAtZeroHourUTC } from '@/lib/date/days';
 
 import CommitmentCard from '../card/commitment-card';
 import { Filters } from '../types';
 
+type ListCommitment = Pick<Commitment, 'id' | 'title' | 'status' | 'doneBy'>;
+
 interface Props {
-    commitments: Pick<Commitment, 'id' | 'title' | 'status' | 'doneBy'>[];
+    commitments: ListCommitment[];
     filters: Filters
 }
 
-export default function CommitmentsList({ commitments }: Props) {
+export default function CommitmentsList({ commitments, filters }: Props) {
     const [parent] = useAutoAnimate();
+    const [filteredCommitments, setFilteredCommitments] = useState([] as ListCommitment[]);
+
+    useEffect(() => {
+        const emptyFilter = () => true;
+        const filtersToApply = [
+            filters.past ? isPast(todayAtZeroHourUTC()) : emptyFilter,
+            filters.notDone ? isNotDone : emptyFilter
+        ];
+        const isShown = (commitment: ListCommitment) => filtersToApply
+            .map((filterFunction) => filterFunction(commitment))
+            .every((filterResult) => filterResult === true);
+
+        const filtered = commitments.reduce((acc, commitment) => [
+            ...acc,
+            ...isShown(commitment) ? [commitment] : []
+        ], [] as ListCommitment[]);
+
+        setFilteredCommitments(filtered);
+    }, [commitments, filters]);
 
     return (
         <ul ref={parent} role="list" className="divide-y divide-gray-700">
-            {commitments.map((commitment) => <CommitmentCard key={commitment.id}
+            {filteredCommitments.map((commitment) => <CommitmentCard key={commitment.id}
                 commitment={{ ...commitment, doneBy: commitment.doneBy.toString() }}
             />)}
         </ul>
