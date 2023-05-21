af8cbe8d42778f925536c5296d490b29a7289efa
Lean <leian1306@gmail.com>
2023-05-21 15:24:53 +0200

    user moods: restructure the layout and add a loading skeleton

---
+++ src/app/(user)/moods/loading.tsx
@@ -0,0 +1,11 @@
+export default function Loading() {
+    return (
+        <div className="flex animate-pulse gap-10">
+            <div>
+                <div className="mb-5 h-52 w-full rounded-md bg-gray-800 sm:min-w-[250px]"></div>
+                <div className="h-8 w-32 rounded-md bg-gray-800"></div>
+            </div>
+            <div className="mb-5 h-64 w-full rounded-md bg-gray-800 sm:max-w-2xl"></div>
+        </div>
+    );
+}
