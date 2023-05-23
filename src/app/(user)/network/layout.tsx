c9534df92dec1a1fa15ea6801a94016c9d0c9abe
Nicolas Baglivo <nicolas.baglivo@gmail.com>
2023-05-23 22:29:11 +0200

    app (user) network layout: rename page title

--- src/app/(user)/network/layout.tsx
+++ src/app/(user)/network/layout.tsx
@@ -4,7 +4,7 @@ import PageTitle from '@/app/components/page-title';
 export default async function NetworkLayout({ children }: { children: React.ReactNode }) {
     return (
         <main>
-            <PageTitle text="Network" />
+            <PageTitle text="Your network" />
             {children}
         </main>
     );
