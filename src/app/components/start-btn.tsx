81c735c1b343848e7957fe48f9e8eb245682bd64
Nicolas Baglivo <nicolas.baglivo@gmail.com>
2023-05-25 21:58:07 +0200

    src app (user) page: remove meeting card since it was not really usable. We can bring it back once we start working on that feature

--- src/app/components/start-btn.tsx
+++
@@ -1,9 +0,0 @@
-import { AiFillPlayCircle } from 'react-icons/ai';
-
-export default function StartButton() {
-    return (
-        <button type="button">
-            <AiFillPlayCircle size={40} />
-        </button>
-    );
-}
