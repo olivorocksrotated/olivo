c4ddf3f09b90f456cbb7361d4967ca053e5e528d
Nicolas Baglivo <nicolas.baglivo@gmail.com>
2023-05-26 01:00:11 +0200

    src app: fix background colors

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -17,7 +17,7 @@ export default function SignIn() {
     const loader = showLoader ? <div><Loader /></div> : undefined;
 
     return (
-        <div className="flex h-screen w-screen flex-col items-center gap-20 py-64">
+        <div className={`flex h-screen w-screen flex-col items-center gap-20 py-64 ${styles.background}`}>
             <div className={`${styles.logo} min-h-[90px] rounded-md bg-gradient-to-r from-transparent via-purple-400 to-transparent px-64 py-5 text-5xl font-bold`}>
                 OLIVO
             </div>
