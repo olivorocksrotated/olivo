f0d0fba64e0b982b570141f3b869a144efee827e
JoseSPV <jose.sancho@fashion.cloud>
2023-05-27 15:09:27 +0200

    stc app user feedback: implement styles for user selection and continue to next step button

--- src/app/(user)/feedback/page.tsx
+++ src/app/(user)/feedback/page.tsx
@@ -1,29 +1,59 @@
 'use client';
 
-const feedbackEntries = [{
-    id: 1,
-    receiverId: 1,
-    type: 'positive',
-    categories: [{ name: 'communication', tags: ['meh'] }, { name: 'teamwork', tags: ['needs more'] }],
-    comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-}];
+import { TbSearch } from 'react-icons/tb';
 
-const networkUsers = [{}];
+import { getFeedbackCategories } from '@/lib/feedback/categories';
 
-export default async function Feedback() {
-    console.log(feedbackEntries);
-    console.log(networkUsers);
+export default function Feedback() {
+    const categories = getFeedbackCategories();
+    const users = [
+        { id: '1', name: 'Super Doggo', role: 'Engineering Manager', portait: 'https://placedog.net/500/500' },
+        { id: '2', name: 'The Boss', role: 'Engineering Manager', portait: 'https://placedog.net/300/500' }
+    ];
 
     return (
         <main>
             <section className="max-w-lg">
                 {/* User selection */}
-                <div>
-                    <input type="search" placeholder="User name"></input>
-                    <ul>
-                        <li>Lean</li>
-                        <li>Nico</li>
-                    </ul>
+                <div className="bg-slate-700 p-3">
+                    <div className="flex rounded-t-md border border-slate-700 bg-gray-600/20">
+                        <span className="p-3 py-4"><TbSearch size={16} /></span>
+                        <input type="email" name="email" id="email" placeholder="Search Review" className="bg-transparent p-2 focus:outline-none"></input>
+                    </div>
+                </div>
+
+                <div className="mb-4 rounded-b-md bg-slate-800 p-3">
+                    {users.map((user) => (
+                        <div key={user.id} className="px-0 pb-10">
+                            <div className="mx-0 px-0 pb-0 pt-5">
+                                <div className="sm:flex sm:items-center sm:justify-between sm:space-x-5">
+                                    <div className="flex min-w-0 flex-1 items-center">
+                                        <img alt={user.name} src={user.portait} className="h-10 w-10 shrink-0 rounded-full object-cover" />
+                                        <div className="my-0 ml-4 mr-0 min-w-0 flex-1">
+                                            <div>
+                                                <span className="text-lg font-bold text-slate-200">{user.name}</span>
+                                            </div>
+
+                                            <div>
+                                                <span className="text-base text-slate-300">{user.role}</span>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <a href="#_" className="group relative inline-flex items-center justify-center overflow-hidden rounded-md px-6 py-3 font-bold text-white shadow-2xl">
+                                        <span className="absolute inset-0 h-full w-full bg-gradient-to-br from-pink-600 via-purple-700 to-blue-400 opacity-0 transition duration-300 ease-out group-hover:opacity-100"></span>
+                                        <span className="absolute left-0 top-0 h-1/3 w-full bg-gradient-to-b from-white to-transparent opacity-5"></span>
+                                        <span className="absolute bottom-0 left-0 h-1/3 w-full bg-gradient-to-t from-white to-transparent opacity-5"></span>
+                                        <span className="absolute bottom-0 left-0 h-full w-4 bg-gradient-to-r from-white to-transparent opacity-5"></span>
+                                        <span className="absolute bottom-0 right-0 h-full w-4 bg-gradient-to-l from-white to-transparent opacity-5"></span>
+                                        <span className="absolute inset-0 h-full w-full rounded-md border border-white opacity-10"></span>
+                                        <span className="absolute h-0 w-0 rounded-full bg-white opacity-5 transition-all duration-300 ease-out group-hover:h-56 group-hover:w-56"></span>
+                                        <span className="relative">Choose</span>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
                 </div>
 
                 {/* Feedback type selection */}
@@ -33,11 +63,17 @@ export default async function Feedback() {
                 </div>
 
                 {/* Feedback categories selection */}
-                <div className="flex gap-4">
-                    <div className="h-40 w-40 bg-slate-400">Communication</div>
-                    <div className="h-40 w-40 bg-slate-400">Teamwork</div>
-                    <div className="h-40 w-40 bg-slate-400">Leadership</div>
-                    <div className="h-40 w-40 bg-slate-400">Problem solving</div>
+                <h2 className="font-bold">Feedback Categories</h2>
+                <div className="relative flex flex-wrap gap-4">
+                    {
+                        categories.map((category) => (
+                            <div key={category.id} className="">
+                                <div className="absolute bottom-4 left-3  text-neutral-200">
+                                    <h3>{category.name}</h3>
+                                </div>
+                            </div>
+                        ))
+                    }
                 </div>
 
                 {/* Feedback tags for selected categories */}
@@ -67,7 +103,15 @@ export default async function Feedback() {
                 </div>
 
                 <div>
-                    <div>Save</div>
+                    <div>
+                        <a href="#_" className="group relative inline-flex items-center justify-center overflow-hidden rounded-md p-0.5 font-bold">
+                            <span className="absolute h-full w-full bg-gradient-to-br from-[#ff8a05] via-[#ff5478] to-[#ff00c6] group-hover:from-[#ff00c6] group-hover:via-[#ff5478] group-hover:to-[#ff8a05]"></span>
+
+                            <span className="relative rounded-md bg-gray-900 px-6 py-3 transition-all duration-500 ease-out group-hover:bg-gray-900/0">
+                                <span className="relative text-slate-200">Next step</span>
+                            </span>
+                        </a>
+                    </div>
                 </div>
             </section>
         </main>
