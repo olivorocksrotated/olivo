83710438b024252d56d7cd87ffb9390e52ebf3c4
Lean <leian1306@gmail.com>
2023-05-27 19:16:28 +0200

    pages api auth callbacks jwt: create a welcome notification on sign up

--- src/pages/api/auth/_callbacks/jwt.ts
+++ src/pages/api/auth/_callbacks/jwt.ts
@@ -1,8 +1,18 @@
+import { NotificationType } from '@prisma/client';
 import { Account, Profile, User } from 'next-auth';
 import { AdapterUser } from 'next-auth/adapters';
 import { JWT } from 'next-auth/jwt';
 
-export default async function jwtCallback({ token, isNewUser }: {
+import { createNotification } from '@/lib/notifications/persistent/create';
+
+function handleSignup(user: User | AdapterUser) {
+    createNotification(user.id, {
+        title: '🎉 Welcome to Olivo!',
+        type: NotificationType.SignupWelcome
+    });
+}
+
+export default async function jwtCallback({ token, user, trigger, isNewUser }: {
     token: JWT;
     user: User | AdapterUser;
     account: Account | null;
@@ -11,5 +21,9 @@ export default async function jwtCallback({ token, isNewUser }: {
     isNewUser?: boolean | undefined;
     session?: any;
 }) {
+    if (trigger === 'signUp') {
+        handleSignup(user);
+    }
+
     return { ...token, isNewUser: !!isNewUser };
 }
