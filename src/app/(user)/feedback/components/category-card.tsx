485c40b3c6f180ce85b58a5120c16ef09fb3072c
JoseSPV <jose.sancho@fashion.cloud>
2023-05-29 01:18:09 +0200

    src app user feedback components: stlye adjustments

--- src/app/(user)/feedback/components/category-card.tsx
+++ src/app/(user)/feedback/components/category-card.tsx
@@ -14,7 +14,7 @@ export default function CategoryCard({ category }: Props) {
     };
 
     return (
-        <div className="relative flex h-40 w-40 cursor-pointer flex-col justify-end overflow-hidden rounded-md bg-slate-700 shadow-lg" onClick={handleClick}>
+        <div className={`relative flex h-40 w-40 cursor-pointer flex-col justify-end overflow-hidden rounded-md border shadow-md ${isClicked ? 'border-teal-600  bg-slate-700 shadow-slate-700' : 'border-slate-500'}`} onClick={handleClick}>
             <div className={`absolute right-2 top-2 flex h-8 w-8 items-center justify-center rounded-full border ${isClicked ? 'border-teal-600' : 'border-slate-500'}`}>
                 <svg className={`h-4 w-4 text-teal-600 ${isClicked ? 'opacity-100' : 'opacity-0'} transition-opacity duration-300`}
                     fill="none"
@@ -25,7 +25,7 @@ export default function CategoryCard({ category }: Props) {
                 </svg>
             </div>
 
-            <div className="pb-3 pl-3 font-semibold">{category}</div>
+            <div className={`pb-3 pl-3 font-semibold ${isClicked ? 'text-slate-100' : 'text-slate-500'}`}>{category}</div>
         </div>
     );
 }
--- src/app/(user)/feedback/components/feedback-type-selector.tsx
+++ src/app/(user)/feedback/components/feedback-type-selector.tsx
@@ -4,13 +4,13 @@ import { TbRocket, TbSchoolBell } from 'react-icons/tb';
 
 export default function FeedbackTypeSelector() {
     return (
-        <div className="relative mb-4 flex justify-between gap-4">
-            <div className="mb-1 mr-1 flex h-[150px] w-[200px] cursor-pointer flex-col flex-wrap justify-center gap-y-4 rounded border border-slate-700 px-8 py-3 pl-4 font-bold uppercase text-slate-500 outline-none transition-all duration-150 ease-linear hover:bg-teal-700 hover:text-slate-100">
+        <div className="relative mb-4 flex flex-nowrap gap-5">
+            <div className="flex h-[150px] w-[245px] cursor-pointer flex-col flex-wrap justify-center rounded border border-slate-700 px-8 py-3 pl-4 font-bold uppercase text-slate-500 outline-none transition-all duration-150 ease-linear after:border-t after:border-slate-700 hover:bg-teal-700 hover:text-slate-100 hover:after:border-slate-100">
                 <div><TbRocket fontSize={32} /></div>
                 <h3 className="text-xl font-bold">Praise</h3>
             </div>
 
-            <div className="mb-1 mr-1 flex h-[150px] w-[242px] cursor-pointer flex-col flex-wrap justify-center gap-y-4 rounded border border-slate-700 px-8 py-3 pl-4 font-bold uppercase text-slate-500 outline-none transition-all duration-150 ease-linear hover:bg-amber-700 hover:text-slate-100">
+            <div className="flex h-[150px] w-[245px] cursor-pointer flex-col flex-wrap justify-center rounded border border-slate-700 px-8 py-3 pl-4 font-bold uppercase text-slate-500 outline-none transition-all duration-150 ease-linear after:border-t after:border-slate-700 hover:bg-amber-700 hover:text-slate-100 hover:after:border-slate-100">
                 <div><TbSchoolBell fontSize={32} /></div>
                 <h3 className="text-xl font-bold">Improve</h3>
             </div>
