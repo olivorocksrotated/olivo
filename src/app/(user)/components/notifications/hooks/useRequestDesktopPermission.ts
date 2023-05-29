d3235ae372906288dd3537fbd16d224e4f77815a
Lean <leian1306@gmail.com>
2023-05-29 13:48:03 +0200

    lib notifications desktop: change the way the Notification check is done to be safer, and make the API extra safe by always checking for support. Also remove unused hook

--- src/app/(user)/components/notifications/hooks/useRequestDesktopPermission.ts
+++ src/app/(user)/components/notifications/hooks/useRequestDesktopPermission.ts
@@ -1,11 +1,9 @@
 import { useEffect } from 'react';
 
-import { isDesktopNotificationGranted, requestDesktopNotificationPermission } from '@/lib/notifications/desktop';
+import { requestDesktopNotificationPermission } from '@/lib/notifications/desktop';
 
 export default function useRequestDesktopPermission() {
     useEffect(() => {
-        if (!isDesktopNotificationGranted()) {
-            requestDesktopNotificationPermission();
-        }
+        requestDesktopNotificationPermission();
     }, []);
 }
