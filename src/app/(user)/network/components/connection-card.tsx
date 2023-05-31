8f7151ebb5c9f72270378a6e262d1c1335fb29d6
Nicolas Baglivo <nicolas.baglivo@gmail.com>
2023-05-31 19:53:48 +0200

    app (user) network components connection-card: make it a link to the connection

--- src/app/(user)/network/components/connection-card.tsx
+++ src/app/(user)/network/components/connection-card.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { motion } from 'framer-motion';
+import Link from 'next/link';
 
 import Avatar from './avatar';
 
@@ -14,21 +15,23 @@ interface Props {
 
 export default function Connection({ connection }: Props) {
     return (
-        <motion.div whileHover={{ scale: 1.05 }}
-            transition={{ type: 'spring', stiffness: 200, damping: 10 }}
-            className="flex w-full min-w-[300px] cursor-pointer gap-4 border border-neutral-900 bg-neutral-950 p-5"
-        >
-            <div>
-                <Avatar connection={connection} />
-            </div>
-            <div>
-                <div className="text-lg font-extralight text-neutral-100" data-cy="connection-user-name">
-                    {connection.name || 'No name'}
+        <Link href={`/network/${connection.id}`} className="w-full min-w-[300px]">
+            <motion.div whileHover={{ scale: 1.05 }}
+                transition={{ type: 'spring', stiffness: 200, damping: 10 }}
+                className="flex  cursor-pointer gap-4 border border-neutral-900 bg-neutral-950 p-5"
+            >
+                <div>
+                    <Avatar connection={connection} />
                 </div>
-                <div className="text-sm font-light text-neutral-400">
-                    Human Being
+                <div>
+                    <div className="text-lg font-extralight text-neutral-100" data-cy="connection-user-name">
+                        {connection.name || 'No name'}
+                    </div>
+                    <div className="text-sm font-light text-neutral-400">
+                        Human Being
+                    </div>
                 </div>
-            </div>
-        </motion.div>
+            </motion.div>
+        </Link>
     );
 }
