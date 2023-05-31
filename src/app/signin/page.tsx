ece13d93e12cf39dc706f01b2281e8a290c234aa
Lean <leian1306@gmail.com>
2023-05-31 10:20:11 +0200

    signing page: add the version of the app

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -7,6 +7,7 @@ import { VscGithub } from 'react-icons/vsc';
 
 import { isDevEnvironment } from '@/lib/environment';
 
+import packageInfo from '../../../package.json';
 import Loader from '../components/loader';
 import LoginButton from './components/login-btn';
 import styles from './page.module.css';
@@ -25,10 +26,11 @@ export default function SignIn() {
 
     return (
         <div className={`flex h-screen w-screen flex-col items-center gap-20 py-64 ${styles.background}`}>
+            <div className="absolute right-4 top-4 text-neutral-300">version {packageInfo.version}</div>
             <div className={`${styles.logo} min-h-[90px] rounded-md bg-gradient-to-r from-transparent via-purple-400 to-transparent px-64 py-5 text-5xl font-bold`}>
                 OLIVO
             </div>
-            <div className="flex gap-4">
+            <div className="flex flex-col gap-4 sm:flex-row">
                 <LoginButton provider="github" onLoginAttempt={handleLoginAttempt}>
                     <div className="flex items-center gap-2"><VscGithub size={20} /> Login with Github</div>
                 </LoginButton>
