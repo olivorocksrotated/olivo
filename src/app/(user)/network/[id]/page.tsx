05c5feb600f25730d1127fb448c3a5bd88ef516f
Nicolas Baglivo <nicolas.baglivo@gmail.com>
2023-05-31 21:49:43 +0200

    src app (user) network [id] page: create initial connection page

---
+++ src/app/(user)/network/[id]/page.tsx
@@ -0,0 +1,33 @@
+import { notFound } from 'next/navigation';
+
+import Button from '@/app/components/button';
+import { getConnection } from '@/lib/network/connection/get';
+
+import Avatar from '../components/avatar/avatar';
+import { AvatarSize } from '../components/avatar/types';
+
+export default async function ConnectionPage({ params }: { params: { id: string } }) {
+    const connection = await getConnection(params.id);
+
+    if (!connection) {
+        return notFound();
+    }
+
+    return (
+        <div className="flex h-full justify-between">
+            <div className="flex gap-4">
+                <div>
+                    <Avatar size={AvatarSize.Big} user={connection.user} />
+                </div>
+                <div>
+                    <div className="text-2xl font-extralight text-neutral-100">
+                        {connection.user.name || 'No name'}
+                    </div>
+                </div>
+            </div>
+            <div>
+                <Button>Disconnect</Button>
+            </div>
+        </div>
+    );
+}
