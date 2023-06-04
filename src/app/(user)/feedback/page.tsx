ea0af4e640796c2170b9fdb7e1a1d70b1d26214b
JoseSPV <jose.sancho@fashion.cloud>
2023-06-04 17:06:06 +0200

    src app user feedback: refine animations and move values to a reusable variable

--- src/app/(user)/feedback/page.tsx
+++ src/app/(user)/feedback/page.tsx
@@ -27,77 +27,88 @@ export default function Feedback() {
     const categories = getFeedbackCategories();
     const feedbackSuggestionTags = getFeedbackSuggestionTags();
 
+    const animationProps = {
+        initial: { x: 10, opacity: 0 },
+        animate: { x: 0, opacity: 1 },
+        exit: { x: -10, opacity: 0 }
+    };
     const transition = {
-        duration: 0.7
+        duration: 0.2
     };
 
     return (
         <main>
-            <section className="min-h-[512px] max-w-lg overflow-hidden">
-                <FeedbackStepper step={feedbackStep} stepTitle={feedbackSteps[feedbackStep]} />
+            <section className="min-h-[512px] max-w-lg">
+                <div>
+                    <div>
+                        <FeedbackStepper step={feedbackStep} stepTitle={feedbackSteps[feedbackStep]} />
+                    </div>
 
-                <AnimatePresence mode="wait">
+                    <div className="w-full overflow-hidden">
+                        <AnimatePresence mode="wait">
 
-                    {feedbackStep === 1 &&
-                            <motion.div key={feedbackStep ? feedbackStep : ''} animate={{ opacity: 1 }} initial={{ opacity: 0 }} exit={{ opacity: 0, translateX: '-100%' }} transition={transition}>
-                                <UserSelector />
-                            </motion.div>}
+                            {feedbackStep === 1 &&
+                                    <motion.div key={feedbackStep ? feedbackStep : ''} animate={animationProps.animate} initial={animationProps.initial} exit={animationProps.exit} transition={transition}>
+                                        <UserSelector />
+                                    </motion.div>}
 
-                    {feedbackStep === 2 &&
-                            <motion.div key={feedbackStep ? feedbackStep : ''} animate={{ opacity: 1 }} initial={{ opacity: 0 }} exit={{ opacity: 0, translateX: '-100%' }} transition={transition}>
-                                <FeedbackTypeSelector />
-                            </motion.div>}
+                            {feedbackStep === 2 &&
+                                    <motion.div key={feedbackStep ? feedbackStep : ''} animate={animationProps.animate} initial={animationProps.initial} exit={animationProps.exit} transition={transition}>
+                                        <FeedbackTypeSelector />
+                                    </motion.div>}
 
-                    {feedbackStep === 3 &&
-                            <motion.div key={feedbackStep ? feedbackStep : ''} animate={{ opacity: 1 }} initial={{ opacity: 0 }} exit={{ opacity: 0, translateX: '-100%' }} transition={transition}>
-                                <div className="relative mb-4 flex flex-wrap gap-4">
-                                    {
-                                        categories.map((category) => (
-                                            <CategoryCard key={category.id} category={category.name} />
-                                        ))
-                                    }
+                            {feedbackStep === 3 &&
+                                    <motion.div key={feedbackStep ? feedbackStep : ''} animate={animationProps.animate} initial={animationProps.initial} exit={animationProps.exit} transition={transition}>
+                                        <div className="relative mb-4 flex flex-wrap gap-4">
+                                            {
+                                                categories.map((category) => (
+                                                    <CategoryCard key={category.id} category={category.name} />
+                                                ))
+                                            }
+                                        </div>
+                                    </motion.div>}
+
+                            {feedbackStep === 4 &&
+                                    <motion.div key={feedbackStep ? feedbackStep : ''} animate={animationProps.animate} initial={animationProps.initial} exit={animationProps.exit} transition={transition}>
+                                        <div className="mb-4 flex flex-wrap gap-4">
+                                            {feedbackSuggestionTags.map((tag) => (
+                                                <CategoryBatchSelector key={tag.id} tag={tag.name} />
+                                            ))}
+                                        </div>
+                                    </motion.div>}
+
+                            {feedbackStep === 5 &&
+                                    <motion.div key={feedbackStep ? feedbackStep : ''} animate={animationProps.animate} initial={animationProps.initial} exit={animationProps.exit} transition={transition}>
+                                        <div className="mb-4 h-[150px] w-full rounded-md bg-slate-800 p-3">
+                                            <textarea className="h-full w-full resize-none bg-gray-600/20 p-2 text-slate-100 outline-none" placeholder="Leave a comment"></textarea>
+                                        </div>
+                                    </motion.div>}
+
+
+                        </AnimatePresence>
+                    </div>
+
+                    <div className="flex flex-wrap justify-between gap-y-2">
+                        {feedbackStep > 1 &&
+                            <div className="cursor-pointer">
+                                <div onClick={previousStep} className="group relative inline-flex items-center justify-center overflow-hidden rounded-md p-0.5 font-bold">
+                                    <span className="absolute h-full w-full bg-gradient-to-br from-[#ff8a05] via-[#ff5478] to-[#ff00c6] group-hover:from-[#ff00c6] group-hover:via-[#ff5478] group-hover:to-[#ff8a05]"></span>
+
+                                    <span className="relative rounded-md bg-gray-900 px-6 py-3 transition-all duration-500 ease-out group-hover:bg-gray-900/0">
+                                        <span className="relative text-slate-200">Previous step</span>
+                                    </span>
                                 </div>
-                            </motion.div>}
+                            </div>}
 
-                    {feedbackStep === 4 &&
-                            <motion.div key={feedbackStep ? feedbackStep : ''} animate={{ opacity: 1 }} initial={{ opacity: 0 }} exit={{ opacity: 0, translateX: '-100%' }} transition={transition}>
-                                <div className="mb-4 flex flex-wrap gap-4">
-                                    {feedbackSuggestionTags.map((tag) => (
-                                        <CategoryBatchSelector key={tag.id} tag={tag.name} />
-                                    ))}
-                                </div>
-                            </motion.div>}
-
-                    {feedbackStep === 5 &&
-                            <motion.div key={feedbackStep ? feedbackStep : ''} animate={{ opacity: 1 }} initial={{ opacity: 0 }} exit={{ opacity: 0, translateX: '-100%' }} transition={transition}>
-                                <div className="mb-4 h-[150px] w-full rounded-md bg-slate-800 p-3">
-                                    <textarea className="h-full w-full resize-none bg-gray-600/20 p-2 text-slate-100 outline-none" placeholder="Leave a comment"></textarea>
-                                </div>
-                            </motion.div>}
-
-
-                </AnimatePresence>
-
-                <div className="flex flex-nowrap justify-between">
-                    {feedbackStep > 1 &&
                         <div className="cursor-pointer">
-                            <div onClick={previousStep} className="group relative inline-flex items-center justify-center overflow-hidden rounded-md p-0.5 font-bold">
+                            <div onClick={nextStep} className="group relative inline-flex items-center justify-center overflow-hidden rounded-md p-0.5 font-bold">
                                 <span className="absolute h-full w-full bg-gradient-to-br from-[#ff8a05] via-[#ff5478] to-[#ff00c6] group-hover:from-[#ff00c6] group-hover:via-[#ff5478] group-hover:to-[#ff8a05]"></span>
 
                                 <span className="relative rounded-md bg-gray-900 px-6 py-3 transition-all duration-500 ease-out group-hover:bg-gray-900/0">
-                                    <span className="relative text-slate-200">Previous step</span>
+                                    {feedbackStep < 5 && <span className="relative text-slate-200">Next step</span>}
+                                    {feedbackStep === 5 && <span className="relative text-slate-200">Save!</span>}
                                 </span>
                             </div>
-                        </div>}
-
-                    <div className="cursor-pointer">
-                        <div onClick={nextStep} className="group relative inline-flex items-center justify-center overflow-hidden rounded-md p-0.5 font-bold">
-                            <span className="absolute h-full w-full bg-gradient-to-br from-[#ff8a05] via-[#ff5478] to-[#ff00c6] group-hover:from-[#ff00c6] group-hover:via-[#ff5478] group-hover:to-[#ff8a05]"></span>
-
-                            <span className="relative rounded-md bg-gray-900 px-6 py-3 transition-all duration-500 ease-out group-hover:bg-gray-900/0">
-                                {feedbackStep < 5 && <span className="relative text-slate-200">Next step</span>}
-                                {feedbackStep === 5 && <span className="relative text-slate-200">Save!</span>}
-                            </span>
                         </div>
                     </div>
                 </div>
