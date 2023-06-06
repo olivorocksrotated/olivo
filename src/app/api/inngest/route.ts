7a59239e54dac4c88aad555d63a36dedfedc90c7
Lean <leian1306@gmail.com>
2023-06-06 22:26:52 +0200

    app api inngest route: remove unnecessary comment

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -4,7 +4,6 @@ import { createSignupWelcomeNotification } from '@/inngest/functions';
 
 import { inngest } from '../../../inngest/client';
 
-// Create an API that serves zero functions
 export const { GET, POST, PUT } = serve(inngest, [
     createSignupWelcomeNotification
 ]);
