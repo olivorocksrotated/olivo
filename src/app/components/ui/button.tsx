272a0ff28ef32e011635624207fb1c8461fe8bc0
Lean <leian1306@gmail.com>
2023-06-11 22:46:49 +0200

    components ui button: add secondary button

--- src/app/components/ui/button.tsx
+++ src/app/components/ui/button.tsx
@@ -6,19 +6,18 @@ import { IconType } from 'react-icons';
 import styles from './button.module.css';
 
 const buttonStyles = cva(
-    'flex items-center justify-between gap-2 rounded border border-solid px-4 py-1 disabled:cursor-not-allowed disabled:opacity-50',
+    'flex items-center justify-between gap-2 rounded border border-solid border-neutral-500 px-4 py-1 text-white disabled:cursor-not-allowed disabled:opacity-50',
     {
         variants: {
             intent: {
-                default: 'border-neutral-500 text-white transition hover:enabled:border-neutral-200',
+                default: 'transition hover:enabled:border-neutral-200',
                 cta: `${styles.glow} hover:enabled:after:bg-neutral-950`,
-                secondary: ''
+                secondary: 'text-white transition hover:enabled:bg-neutral-950'
             }
         },
         defaultVariants: {
             intent: 'default'
-        },
-        compoundVariants: [{}]
+        }
     }
 );
 
@@ -31,7 +30,7 @@ export default function Button(props: Props) {
     const { label, icon: Icon, ...nativeProps } = props;
 
     return (
-        <button type="button" {...nativeProps} className={buttonStyles(props)}>
+        <button type="button" aria-label={label} {...nativeProps} className={buttonStyles(props)}>
             {Icon ? <Icon size={20} /> : null}
             <span>{label}</span>
         </button>
