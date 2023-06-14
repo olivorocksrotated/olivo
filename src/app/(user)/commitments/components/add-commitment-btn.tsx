8dfa84babb74f8237e08e3f23fbe4f67c65aeb3f
Lean <leian1306@gmail.com>
2023-06-14 19:24:13 +0200

    user commitments components add: close the modal after action

--- src/app/(user)/commitments/components/add-commitment-btn.tsx
+++ src/app/(user)/commitments/components/add-commitment-btn.tsx
@@ -7,7 +7,7 @@ import { useZact } from 'zact/client';
 
 import Button from '@/app/components/ui/button/button';
 import IconButton from '@/app/components/ui/icon-button/icon-button';
-import Modal from '@/app/components/ui/modal/modal';
+import Modal, { setModalClosed } from '@/app/components/ui/modal/modal';
 import modalStyles from '@/app/components/ui/modal/modal.module.css';
 import { createCommitmentAction } from '@/lib/commitments/create';
 import { dateInputToISOString, formatDate } from '@/lib/date/format';
@@ -15,19 +15,22 @@ import { dateInputToISOString, formatDate } from '@/lib/date/format';
 export default function AddCommitmentButton() {
     const nullCommitment = { title: '', doneBy: formatDate(new Date(), 'yyyy-MM-dd') };
     const [commitment, setCommitment] = useState(nullCommitment);
+    const [closeAddModal, setCloseAddModal] = useState(false);
 
     const { mutate: createCommitment } = useZact(createCommitmentAction);
 
-    async function onSubmit(event: MouseEvent<HTMLButtonElement>) {
+    const onSubmit = async (event: MouseEvent<HTMLButtonElement>) => {
         event.preventDefault();
         if (commitment.title && commitment.doneBy) {
             await createCommitment({ ...commitment, doneBy: dateInputToISOString(commitment.doneBy)! });
+            setModalClosed(setCloseAddModal);
             setCommitment(nullCommitment);
         }
-    }
+    };
 
     return (
         <Modal title="Add commitment"
+            close={closeAddModal}
             onClose={() => setCommitment(nullCommitment)}
             openComponent={<IconButton icon={IoAddOutline} label="Add commitment" />}
         >
@@ -63,41 +66,6 @@ export default function AddCommitmentButton() {
                 />
             </div>
         </Modal>
-
-    // <DialogButton onSubmit={onSubmit}
-    //     onClose={() => setCommitment(nullCommitment)}
-    //     dialog={{
-    //         title: 'Add commitment',
-    //         actionLabel: 'Add',
-    //         actionDisabled: !commitment.title || !commitment.doneBy
-    //     }}
-    //     openButton={<IconButton icon={IoAddOutline} label="Add commitment" />}
-    // >
-    //     <div>
-    //         <div className="mb-4 flex items-center">
-    //             <span className="w-16">I will</span>
-    //             <input value={commitment.title}
-    //                 autoFocus
-    //                 onChange={(event) => setCommitment({ ...commitment, title: event.target.value })}
-    //                 placeholder="e.g. do this task"
-    //                 className="inline-flex h-8 w-full items-center justify-center rounded px-2.5 leading-none outline-none"
-    //             />
-    //         </div>
-    //         <div className="mb-4 flex items-center">
-    //             <span className="w-16">by</span>
-    //             <input type="date"
-    //                 value={commitment.doneBy}
-    //                 onChange={(event) => setCommitment({ ...commitment, doneBy: event.target.value })}
-    //                 placeholder="done by"
-    //                 className="h-8 w-full px-2.5"
-    //             />
-    //         </div>
-    //         <div className="mb-4">
-    //             <span className="text-slate-300">Find all your commitments</span>{' '}
-    //             <Link href="/commitments" className="text-white hover:text-indigo-300">in the &quot;Commitments&quot; section</Link>
-    //         </div>
-    //     </div>
-    // </DialogButton>
     );
 }
 
