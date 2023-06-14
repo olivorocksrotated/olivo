05f1198b393b4f0a0df5d43123716486b550edc4
Lean <leian1306@gmail.com>
2023-06-14 20:11:55 +0200

    user network connect button: use the Modal component

--- src/app/(user)/network/components/connect-button.tsx
+++ src/app/(user)/network/components/connect-button.tsx
@@ -2,45 +2,44 @@
 
 import { MouseEvent, useState } from 'react';
 
-import DialogButton from '@/app/components/dialog-button';
 import Button from '@/app/components/ui/button/button';
+import Modal, { useCloseModal } from '@/app/components/ui/modal/modal';
+import modalStyles from '@/app/components/ui/modal/modal.module.css';
 
 export default function ConnectButton({ onConnectionRequested }: { onConnectionRequested: (email: string) => void }) {
-    const [email, setEmail] = useState<string>();
-
-    async function onSubmit(event: MouseEvent<HTMLButtonElement>) {
-        event.preventDefault();
-        if (email) {
-            onConnectionRequested(email);
-            setEmail('');
-        }
-    }
-
+    const [email, setEmail] = useState('');
+    const [isClosed, closeModal] = useCloseModal();
 
     function reset() {
         setEmail('');
     }
 
+    async function onSubmit(event: MouseEvent<HTMLButtonElement>) {
+        event.preventDefault();
+        if (email) {
+            onConnectionRequested(email);
+            reset();
+            closeModal();
+        }
+    }
+
     return (
-        <DialogButton onClose={reset}
-            onSubmit={onSubmit}
-            dialog={{
-                title: 'Add the email of the user you would like to connect with',
-                actionLabel: 'Connect',
-                actionDisabled: !email
-            }}
-            openButton={<Button label="Connect" />}
+        <Modal title="Connect"
+            description="Add the email of the user you would like to connect with"
+            close={isClosed}
+            openComponent={<Button label="Connect" />}
         >
-            <div className="mb-4">
-                <div className="">
-                    <input id="email"
-                        value={email}
-                        onChange={(event) => setEmail(event.target.value)}
-                        placeholder="email"
-                        className="inline-flex h-8 w-full items-center justify-center rounded px-2.5 leading-none outline-none"
-                    />
-                </div>
+            <div className={modalStyles['modal-content']}>
+                <input type="email"
+                    value={email}
+                    onChange={(event) => setEmail(event.target.value)}
+                    placeholder="email"
+                    className="inline-flex h-8 w-full items-center justify-center rounded px-2.5 leading-none outline-none"
+                />
             </div>
-        </DialogButton>
+            <div className={modalStyles['modal-actions']}>
+                <Button intent="cta" label="Connect" disabled={!email} onClick={onSubmit} />
+            </div>
+        </Modal>
     );
 }
