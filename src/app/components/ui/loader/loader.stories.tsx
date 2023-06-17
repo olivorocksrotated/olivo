0feaf226eeabda6485dd7dcf2fe356eab5cd58f3
Lean <leian1306@gmail.com>
2023-06-17 17:07:51 +0200

    components ui: add a loader component

---
+++ src/app/components/ui/loader/loader.stories.tsx
@@ -0,0 +1,59 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Loader from './loader';
+
+const meta: Meta<typeof Loader> = {
+    title: 'UI/Loader',
+    component: Loader,
+    tags: ['autodocs'],
+    parameters: {
+        docs: {
+            description: {
+                component: 'Loading indicator'
+            }
+        }
+    },
+    argTypes: {
+        intent: {
+            name: 'intent',
+            type: 'string',
+            description: 'The purpose of this loader',
+            control: { type: 'radio' },
+            options: ['standalone', 'inner'],
+            table: {
+                type: { summary: 'standalone | inner' },
+                defaultValue: { summary: 'standalone' }
+            }
+        },
+        size: {
+            name: 'size',
+            type: 'string',
+            description: 'The size of this loader',
+            control: { type: 'radio' },
+            options: ['xs', 's', 'md', 'lg'],
+            table: {
+                type: { summary: 'xs | s | md | lg' },
+                defaultValue: { summary: 's' }
+            }
+        }
+    }
+};
+
+export default meta;
+type Story = StoryObj<typeof Loader>;
+
+export const Default: Story = {
+    args: {}
+};
+
+export const Inner: Story = {
+    args: {
+        intent: 'inner'
+    }
+};
+
+export const Standalone: Story = {
+    args: {
+        intent: 'standalone'
+    }
+};
