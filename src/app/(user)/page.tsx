245d4ac20e9af175a5dff4162b5612edb97f1bf1
Lean <leian1306@gmail.com>
2023-06-20 20:30:36 +0200

    home: display the new list of commitments

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -4,7 +4,7 @@ import { getTodaysMood } from '@/lib/moods/get';
 import { getFirstName } from '@/lib/name/name';
 
 import PageTitle from '../components/ui/page-title/page-title';
-import CommitmentsSection from './commitments/components/commitments-section';
+import NewCommitmentsList from './commitments/components/list/new-commitments-list';
 import MoodSelector from './moods/components/mood-selector';
 
 export default async function Home() {
@@ -22,7 +22,10 @@ export default async function Home() {
         <section>
             <PageTitle text={`👋 Hey, ${firstName}`} />
             <div className="mb-10"><MoodSelector todaysMood={todaysMood} /></div>
-            <div><CommitmentsSection commitments={commitments} title="Commitments for today" /></div>
+            <div className="max-w-2xl rounded-lg p-4 pt-6 outline outline-1 outline-neutral-800">
+                <h2 className="mb-8 text-xl leading-none text-white">Commitments for today</h2>
+                <NewCommitmentsList commitments={commitments} />
+            </div>
         </section>
     );
 }
