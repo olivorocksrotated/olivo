84ee7d866034f4fb7b15239d8b5e7bc858f8b4c3
Lean <leian1306@gmail.com>
2023-06-20 20:04:37 +0200

    commitment status: add abandon to the actions and reuse styles

--- src/app/(user)/commitments/components/card/actions/status-popover.tsx
+++ src/app/(user)/commitments/components/card/actions/status-popover.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { Commitment, CommitmentStatus } from '@prisma/client';
+import clsx from 'clsx';
 
 import { useCloseUiComponent } from '@/app/components/ui/hooks/useCloseUiComponent';
 import Popover from '@/app/components/ui/popover/popover';
@@ -17,6 +18,11 @@ interface Props {
 
 export default function StatusPopover({ commitment, onStatusChange }: Props) {
     const [isClosed, close] = useCloseUiComponent();
+
+    const itemStyles = clsx(
+        'flex cursor-pointer items-center gap-x-2 rounded px-2 py-1',
+        'hover:bg-gray-600'
+    );
 
     const handleStatusChange = (status: CommitmentStatus) => {
         onStatusChange(status);
@@ -36,21 +42,20 @@ export default function StatusPopover({ commitment, onStatusChange }: Props) {
 
     return (
         <Popover close={isClosed} openComponent={openComponent}>
-            <div onClick={() => handleStatusChange(CommitmentStatus.NotStartedYet)}
-                className="mb-2 flex cursor-pointer items-center gap-x-2 rounded px-2 py-1 hover:bg-gray-600"
-            >
-                <NotStartedStatusMarker />
-                <span>Not started</span>
-            </div>
-            <div onClick={() => handleStatusChange(CommitmentStatus.InProgress)}
-                className="mb-2 flex cursor-pointer items-center gap-x-2 rounded px-2 py-1 hover:bg-gray-600"
-            >
-                <InProgressStatusMarker /><span>In progress</span>
-            </div>
-            <div onClick={() => handleStatusChange(CommitmentStatus.Done)}
-                className="flex cursor-pointer items-center gap-x-2 rounded px-2 py-1 hover:bg-gray-600"
-            >
-                <DoneStatusMarker /><span>Done</span>
+            <div className="space-y-2">
+                <div onClick={() => handleStatusChange(CommitmentStatus.NotStartedYet)} className={itemStyles}>
+                    <NotStartedStatusMarker /> Not started
+                </div>
+                <div onClick={() => handleStatusChange(CommitmentStatus.InProgress)} className={itemStyles}>
+                    <InProgressStatusMarker /> In progress
+                </div>
+                <div onClick={() => handleStatusChange(CommitmentStatus.Done)} className={itemStyles}>
+                    <DoneStatusMarker /> Done
+                </div>
+                <div className="w-full border-t border-neutral-600"></div>
+                <div onClick={() => handleStatusChange(CommitmentStatus.Abandoned)} className={itemStyles}>
+                    <AbandonedStatusMarker /> Abandon
+                </div>
             </div>
         </Popover>
     );
