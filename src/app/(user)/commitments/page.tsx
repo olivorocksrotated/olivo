1655873ce0d41a50aeb857d0020b6d32f4a0d8a3
Lean <leian1306@gmail.com>
2023-06-20 20:33:44 +0200

    commitments: display the new list of commitments

--- src/app/(user)/commitments/page.tsx
+++ src/app/(user)/commitments/page.tsx
@@ -1,11 +1,11 @@
 import { getServerSession } from '@/lib/auth/session';
 import { getCommitments } from '@/lib/commitments/get';
 
-import CommitmentsSection from './components/commitments-section';
+import NewCommitmentsList from './components/list/new-commitments-list';
 
 export default async function Commitments() {
     const { user } = await getServerSession();
     const commitments = await getCommitments({ userId: user.id });
 
-    return <CommitmentsSection commitments={commitments} title="All your commitments" />;
+    return <NewCommitmentsList commitments={commitments} />;
 }
