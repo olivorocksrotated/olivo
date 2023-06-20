da7e5b3bb7a4243f33ad03406666ff071e982d6d
Lean <leian1306@gmail.com>
2023-06-20 22:22:35 +0200

    home: sort commitments for easier use

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -1,3 +1,5 @@
+import { CommitmentStatus } from '@prisma/client';
+
 import { getServerSession } from '@/lib/auth/session';
 import { getCommitments } from '@/lib/commitments/get';
 import { getTodaysMood } from '@/lib/moods/get';
@@ -6,6 +8,13 @@ import { getFirstName } from '@/lib/name/name';
 import PageTitle from '../components/ui/page-title/page-title';
 import CommitmentsList from './commitments/components/list/commitments-list';
 import MoodSelector from './moods/components/mood-selector';
+
+const statusWeight = {
+    [CommitmentStatus.InProgress]: 1,
+    [CommitmentStatus.NotStartedYet]: 2,
+    [CommitmentStatus.Done]: 3,
+    [CommitmentStatus.Abandoned]: 4
+};
 
 export default async function Home() {
     const { user } = await getServerSession();
@@ -18,13 +27,15 @@ export default async function Home() {
         order: { createdAt: 'desc' }
     });
 
+    const sortedCommitments = commitments.sort((first, second) => statusWeight[first.status] - statusWeight[second.status]);
+
     return (
         <section>
             <PageTitle text={`👋 Hey, ${firstName}`} />
             <div className="mb-10"><MoodSelector todaysMood={todaysMood} /></div>
             <div className="max-w-2xl rounded-lg p-4 pt-6 outline outline-1 outline-neutral-800">
                 <h2 className="mb-8 text-xl leading-none text-white">Commitments for today</h2>
-                <CommitmentsList commitments={commitments} />
+                <CommitmentsList commitments={sortedCommitments} />
             </div>
         </section>
     );
