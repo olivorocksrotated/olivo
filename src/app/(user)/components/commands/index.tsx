306b74d27d3d30d5dd2b35a953f95675f1f4bf35
Nicolas Baglivo <nicolas.baglivo@gmail.com>
2023-07-07 09:53:10 +0200

    app (user) commponents command-menu commands: add quick-notes command

--- src/app/(user)/components/commands/index.tsx
+++ src/app/(user)/components/commands/index.tsx
@@ -1,7 +1,15 @@
 import { ReactNode } from 'react';
 
+import QuickNotesCommand from './quick-notes';
+
 type CommandDescriptor = {
     [key: string]: { view: ReactNode, title: string, action: { label: string } }
 };
 
-export const Commands: CommandDescriptor = {};
+export const Commands: CommandDescriptor = {
+    'quick-notes': {
+        view: <QuickNotesCommand></QuickNotesCommand>,
+        title: 'Quick note',
+        action: { label: 'Save note' }
+    }
+};
---
+++ src/app/(user)/components/commands/quick-notes.tsx
@@ -0,0 +1,5 @@
+import RichTextEditor from '@/app/components/ui/rich-text-editor/rich-text-editor';
+
+export default function QuickNotesCommand() {
+    return <RichTextEditor autofocus height="md"></RichTextEditor>;
+}
