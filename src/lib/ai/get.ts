46c2a1fa66dee1b86de346e32cb2396deba73a95
Lean <leian1306@gmail.com>
2023-07-29 23:40:00 +0200

    lib ai: add a function to interact with the LLM

--- src/lib/ai/get.ts
+++ src/lib/ai/get.ts
@@ -1,6 +1,7 @@
 import { AiExecutionId } from '@prisma/client';
 
 import prisma from '../prisma';
+import { aiClient } from './client';
 
 export function getAiExecution(executionId: AiExecutionId, createdAfter: Date) {
     return prisma.aiExecution.findFirst({
@@ -9,4 +10,18 @@ export function getAiExecution(executionId: AiExecutionId, createdAfter: Date) {
             createdAt: { gte: createdAfter }
         }
     });
+}
+
+export async function getAiResponse(prompt: string): Promise<string> {
+    try {
+        const response = await aiClient.createChatCompletion({
+            model: 'gpt-4',
+            messages: [{ role: 'user', content: prompt }]
+        });
+
+        return response.data.choices[0].message?.content ?? 'We could not get an answer';
+    } catch (error: any) {
+        const errorMessage = error.response ? `${error.response.status - error.response.data}` : error.message;
+        throw new Error(errorMessage);
+    }
 }
