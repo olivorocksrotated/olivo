00d03152bfdf615a1d73db9ec333dcc268c222b2
Lean <leian1306@gmail.com>
2023-07-30 19:38:35 +0200

    next config: add outputFileTracingExcludes property
    To try to avoid some weird stuff getting into the bundle and exploding the lambda size limit https://github.com/vercel/next.js/issues/42641

--- next.config.js
+++ next.config.js
@@ -2,7 +2,14 @@
 const nextConfig = {
     reactStrictMode: true,
     experimental: {
-        serverActions: true
+        serverActions: true,
+        outputFileTracingExcludes: {
+            '*': [
+                './node_modules/@swc/core-linux-x64-gnu',
+                './node_modules/@swc/core-linux-x64-musl',
+                './node_modules/@esbuild/linux-x64'
+            ]
+        }
     },
     images: {
         remotePatterns: [
