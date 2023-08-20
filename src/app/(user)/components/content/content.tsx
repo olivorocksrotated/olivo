394425ac9b06477f2c68a908bd0836c7a5947645
Nicolas Baglivo <nicolas.baglivo@gmail.com>
2023-08-20 22:26:52 +0200

    Make the main layout scrollable.

--- src/app/(user)/components/content/content.tsx
+++ src/app/(user)/components/content/content.tsx
@@ -3,12 +3,12 @@ import Sidenav from './sidenav/sidenav';
 
 export default function Content({ children }: { children: React.ReactNode }) {
     return (
-        <main className="sm:flex sm:p-6">
+        <main className="h-screen sm:flex sm:p-6">
             <div className="flex items-center justify-between p-3 sm:items-start sm:p-0">
                 <Sidenav />
                 <Notifications />
             </div>
-            <div className="grow p-5 sm:py-0">{children}</div>
+            <div className="max-h-full grow overflow-scroll p-5 sm:py-0">{children}</div>
         </main>
     );
 }
