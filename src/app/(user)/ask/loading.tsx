a12e5b80b73eba7c8f118524af59da8bc35680e9
Lean <leian1306@gmail.com>
2023-08-23 15:59:01 +0200

    ask: improve loading skeleton

--- src/app/(user)/ask/loading.tsx
+++ src/app/(user)/ask/loading.tsx
@@ -1,12 +1,9 @@
 export default function Loading() {
     return (
-        <div className="flex flex-col gap-8 sm:flex-row">
-            <div className="h-fit w-full max-w-xl animate-pulse">
-                <div className="mb-8 h-14 w-80 bg-neutral-700"></div>
-                <div className="mb-4 h-20 w-full bg-neutral-700"></div>
-                <div className="mb-4 h-20 w-full bg-neutral-700"></div>
-                <div className="mb-4 h-20 w-full bg-neutral-700"></div>
-                <div className="mb-4 h-20 w-full bg-neutral-700"></div>
+        <div>
+            <div className="mb-10 flex h-fit w-full animate-pulse flex-wrap gap-4">
+                <div className="mb-4 h-14 w-full bg-neutral-700 sm:w-80"></div>
+                <div className="h-14 w-full bg-neutral-700 sm:w-80"></div>
             </div>
             <div className="h-72 w-full animate-pulse bg-neutral-700"></div>
         </div>
