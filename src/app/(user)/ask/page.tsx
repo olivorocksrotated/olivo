7b5dd4195d7767d091e87b46becd64eaf8345d0b
Lean <leian1306@gmail.com>
2023-08-23 17:27:52 +0200

    ask: display the last execution date

--- src/app/(user)/ask/page.tsx
+++ src/app/(user)/ask/page.tsx
@@ -6,6 +6,7 @@ import { useState } from 'react';
 
 import Button from '@/app/components/ui/button/button';
 import Select, { ItemGroup } from '@/app/components/ui/select/select';
+import { getRelativeDate } from '@/lib/date/format';
 import { fetchFromApi, getApiUrl, ResourcePath } from '@/lib/http/fetch';
 import { HttpMethod } from '@/lib/http/route';
 
@@ -29,6 +30,8 @@ export default function Ask() {
     const [body, setBody] = useState<{
         execution: null | AiExecutionName
     }>({ execution: null });
+
+    const [lastExecutionDate, setLastExecutionDate] = useState<Date | null>(null);
 
     const { messages, isLoading, setInput, setMessages, handleSubmit } = useChat({
         api: getApiUrl(ResourcePath.Ai, ''),
@@ -57,6 +60,7 @@ export default function Ask() {
             role: 'assistant',
             content: lastExecution.response
         }]);
+        setLastExecutionDate(lastExecution.createdAt);
     };
 
     const handleSelectOption = async (executionName: AiExecutionName) => {
@@ -71,6 +75,11 @@ export default function Ask() {
 
         setMessages([]);
         setExecutionName(body.execution);
+    };
+
+    const handleButtonClick = () => {
+        forceSetInputAfterReset();
+        setLastExecutionDate(new Date());
     };
 
     return (
@@ -91,10 +100,13 @@ export default function Ask() {
                         label={!isLoading ? 'Get answer from AI' : 'Generating response...'}
                         loading={isLoading}
                         disabled={isLoading || !body.execution}
-                        onClick={forceSetInputAfterReset}
+                        onClick={handleButtonClick}
                     />
                 </div>
             </form>
+            {lastExecutionDate ? (
+                <p className="mb-2 text-neutral-400">Last execution {getRelativeDate(lastExecutionDate, new Date()).toLowerCase()}</p>
+            ) : null}
             <div className="max-h-96 overflow-y-auto rounded border border-neutral-600 p-4 leading-loose">
                 {messages.length === 0 && !isLoading ? (
                     <div className="text-neutral-600">Your response will appear here</div>
