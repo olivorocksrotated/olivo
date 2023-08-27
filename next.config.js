aecc1048ec1f6726c59db4824ccbc1b78bc52424
Lean <leian1306@gmail.com>
2023-08-27 14:35:06 +0200

    package.json: add a basic bundle analysis tool

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true'
+});
+
 const nextConfig = {
     reactStrictMode: true,
     experimental: {
@@ -29,4 +34,4 @@ const nextConfig = {
     }
 };
 
-module.exports = nextConfig;
+module.exports = withBundleAnalyzer(nextConfig);
