f26ecaf204fc4422dd27cd46079396ffb65e8248
Nicolas Baglivo <nicolas.baglivo@gmail.com>
2023-08-27 14:35:36 +0200

    Fix workspace page layout

--- src/app/(user)/workspace/page.tsx
+++ src/app/(user)/workspace/page.tsx
@@ -7,12 +7,9 @@ export default async function Workspace() {
 
     return (
         <div className="grid h-full grid-cols-2 gap-4 pr-32">
-            <div className="rounded-lg bg-neutral-900 p-5">
+            <div className="flex flex-col rounded-lg bg-neutral-900 p-5">
                 <div className="mb-5 text-xl">Daily Notes</div>
-                <div className="h-full w-full">
-                    <DailyNoteEditor note={note}></DailyNoteEditor>
-                </div>
-
+                <DailyNoteEditor note={note}></DailyNoteEditor>
             </div>
 
             <div className="rounded-lg bg-neutral-900 p-5">
