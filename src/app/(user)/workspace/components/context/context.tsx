8e03ba2f46e3a404d071b5c4bca135fedbb89924
Nicolas Baglivo <nicolas.baglivo@gmail.com>
2023-08-30 22:28:47 +0200

    Implement filtering

--- src/app/(user)/workspace/components/context/context.tsx
+++ src/app/(user)/workspace/components/context/context.tsx
@@ -1,18 +1,19 @@
-'use client';
 import { Note } from '@prisma/client';
-import { useState } from 'react';
+import Link from 'next/link';
 
 import NoteComponent from './note';
 
-export default function Context({ tags, notes }: { tags: string[], notes: Note[] }) {
-    /* eslint-disable @typescript-eslint/no-unused-vars */
-    const [selectedTag, setSelectedTag] = useState<string>('');
-
+export default function Context({ tags, notes, selectedTagFilter }: { tags: string[], notes: Note[], selectedTagFilter?: string }) {
     return (
         <div>
             <div className="my-5 flex flex-wrap gap-4">
                 {tags.map((tag) => (
-                    <div className="cursor-pointer rounded bg-neutral-950 px-2" key={tag} onClick={() => setSelectedTag(tag)}>{tag}</div>
+                    <Link href={{ pathname: '/workspace', query: { selectedTagFilter: tag } }}
+                        className={`cursor-pointer rounded bg-neutral-950 px-2 ${tag === selectedTagFilter ? 'border border-pink-500' : ''}}`}
+                        key={tag}
+                    >
+                        {tag}
+                    </Link>
                 ))}
             </div>
 
--- src/app/(user)/workspace/page.tsx
+++ src/app/(user)/workspace/page.tsx
@@ -5,10 +5,10 @@ import { getTags } from '@/lib/tags/get';
 import Context from './components/context/context';
 import { DailyNoteEditor } from './components/dailyNoteEditor';
 
-export default async function Workspace() {
+export default async function Workspace({ searchParams }: { searchParams: { [key: string]: string | string[] | undefined } }) {
     const note = await getDailyNote();
     const tags = await getTags();
-    const notes = await getNotesWithTag(['rafa']);
+    const notes = await getNotesWithTag(searchParams.selectedTagFilter ? [searchParams.selectedTagFilter as string] : ['rafa']);
 
     return (
         <div className="grid h-full grid-cols-2 gap-4 pr-32">
@@ -19,7 +19,7 @@ export default async function Workspace() {
 
             <div className="rounded-lg bg-neutral-900 p-5">
                 <div className="mb-5 text-xl">Context</div>
-                <Context tags={tags} notes={notes}></Context>
+                <Context selectedTagFilter={searchParams.selectedTagFilter as string} tags={tags} notes={notes}></Context>
             </div>
         </div>
     );
