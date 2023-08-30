625eed7eeb5886404d149e507d9f2a3459c30fdc
Nicolas Baglivo <nicolas.baglivo@gmail.com>
2023-08-30 22:11:56 +0200

    Adjust context filter styles

--- src/app/(user)/workspace/components/context/context.tsx
+++ src/app/(user)/workspace/components/context/context.tsx
@@ -10,9 +10,12 @@ export default function Context({ tags, notes }: { tags: string[], notes: Note[]
 
     return (
         <div>
-            {tags.map((tag) => (
-                <div key={tag} onClick={() => setSelectedTag(tag)}>Tag {tag}</div>
-            ))}
+            <div className="my-5 flex flex-wrap gap-4">
+                {tags.map((tag) => (
+                    <div className="cursor-pointer rounded bg-neutral-950 px-2" key={tag} onClick={() => setSelectedTag(tag)}>{tag}</div>
+                ))}
+            </div>
+
             {notes.map(({ text, id }) => (
                 <NoteComponent key={id} text={text} id={id} ></NoteComponent>
             ))}
