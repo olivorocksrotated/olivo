020a3d55b36c2ec010ae8fccf08e1ace961b052b
Nicolas Baglivo <nicolas.baglivo@gmail.com>
2023-09-17 11:43:09 +0200

    feat(workspace:tag-selector) Improve UX by showing all tags

--- src/app/(user)/components/workspace/components/context/components/tags-selector.tsx
+++ src/app/(user)/components/workspace/components/context/components/tags-selector.tsx
@@ -1,36 +1,43 @@
 'use client';
 
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import Button from '@/app/components/ui/button/button';
 import { useCloseUiComponent } from '@/app/components/ui/hooks/useCloseUiComponent';
 import Popover from '@/app/components/ui/popover/popover';
 
-export default function TagsSelector({ options }: { options: { value: string, label: string }[]}) {
+export default function TagsSelector({ options, selectedValues }: { options: { value: string, label: string }[], selectedValues: string[] }) {
     const pathname = usePathname();
     const searchParams = useSearchParams();
     const router = useRouter();
-    const [tags, setTags] = useState<string[]>([]);
+    const [tags, setTags] = useState(selectedValues);
     const [isClosed, close] = useCloseUiComponent();
+
+    useEffect(() => {
+        setTags(selectedValues);
+    }, [selectedValues]);
 
     function handleSelectOption(tag: string) {
         setTags([...tags, tag]);
     }
 
     function applyTags() {
-        const newSerchParams = new URLSearchParams(searchParams.toString());
-        const currentTags = newSerchParams.get('selectedTagsFilter')?.split(',') || [];
-        newSerchParams.set('selectedTagsFilter', [...currentTags, ...tags].join(','));
-        router.push(`${pathname}?${newSerchParams}`);
+        const query = new URLSearchParams(searchParams.toString());
+        const filterValue = Array.from(new Set(tags));
+        query.set('selectedTagsFilter', filterValue.join(','));
         close();
-        setTags([]);
+        router.push(`${pathname}?${query}`);
+        setTags(filterValue);
     }
 
     return (
         <div className="p-2">
-            <Popover close={isClosed} openComponent={<Button size="md" label="See notes for tag"></Button>}>
-                <div className="flex w-64 flex-wrap gap-2 p-2">
+            <Popover align="center"
+                close={isClosed}
+                openComponent={<Button size="md" label="Select Tags"></Button>}
+            >
+                <div className="max-w-96 flex w-96 flex-wrap gap-2 p-2">
                     {options.length > 0 ? options.map(({ value, label }) => (
                         <div onClick={() => handleSelectOption(value)}
                             key={value}
@@ -38,7 +45,7 @@ export default function TagsSelector({ options }: { options: { value: string, la
                         >
                             {label}
                         </div>
-                    )) : <div>No tags left to apply</div>}
+                    )) : <div>No tags</div>}
                 </div>
                 <div className="flex justify-end">
                     <Button size="s" label="Apply" onClick={applyTags}></Button>
