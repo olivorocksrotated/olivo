8be3d3bece6ae6cb62fc2f503dcb62f67c7d68ac
Nicolas Baglivo <nicolas.baglivo@gmail.com>
2023-09-23 22:00:03 +0200

    feat(home-context): Add better look to context links

--- src/app/(user)/(home)/@context/page.tsx
+++ src/app/(user)/(home)/@context/page.tsx
@@ -1,11 +1,24 @@
-import Link from 'next/link';
+import NextLink from 'next/link';
+import React from 'react';
+
+function Link({ href, children }: { href: string; children: React.ReactNode; }) {
+    return (
+        <div className="rounded-full bg-gradient-to-r from-violet-500 via-blue-500 to-violet-300 p-0.5 text-lg">
+            <div className="flex h-full w-full items-center justify-center rounded-full bg-neutral-800 px-8 py-1">
+                <NextLink href={href}>
+                    {children}
+                </NextLink>
+            </div>
+        </div>
+    );
+}
 
 export default function Context() {
     return (
-        <div className="flex flex-col gap-4">
+        <div className="flex h-3/4 flex-col items-center justify-center gap-4">
             <Link href="/yesterday">Load Yesterday context</Link>
             <Link href="/dynamic">Load Dynamic context</Link>
-            <Link href="/pinned">Load Dynamic context</Link>
+            <Link href="/pinned">Load Pinned context</Link>
         </div>
     );
 }
