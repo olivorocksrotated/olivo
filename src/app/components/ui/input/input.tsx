6535bdf5dd0515cb82092c5c38221d89aab0c9fb
Lean <leian1306@gmail.com>
2023-09-24 21:43:33 +0200

    input: add appearance none to reset native appearance

--- src/app/components/ui/input/input.tsx
+++ src/app/components/ui/input/input.tsx
@@ -1,7 +1,7 @@
 import { cva, VariantProps } from 'cva';
 
 const inputStyles = cva(
-    `h-[42px] min-h-[42px] rounded bg-neutral-600 px-4 py-3 leading-none outline-none
+    `h-[42px] min-h-[42px] appearance-none rounded bg-neutral-600 px-4 py-3 leading-none outline-none
     focus:outline-1 focus:outline-neutral-400
     hover:enabled:outline-1 hover:enabled:outline-neutral-400
     disabled:cursor-not-allowed disabled:opacity-50`,
