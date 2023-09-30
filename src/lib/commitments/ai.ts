5f8540002ef85d82dbd965da66ad0420f426a8c3
Lean <leian1306@gmail.com>
2023-09-30 15:00:41 +0200

    commitments: adjust ai execution prompt

--- src/lib/commitments/ai.ts
+++ src/lib/commitments/ai.ts
@@ -15,13 +15,17 @@ export async function createOvercommitmentPrompt(userId: string): Promise<string
     });
 
     const groupedCommitments = groupCommitmentsByOverdue({ commitments, referenceDate });
+    const hasOverdueCommitments = groupedCommitments.overdue.length > 0;
+    const hasOnTimeCommitments = groupedCommitments.onTime.length > 0;
 
     return `
         Tell me in detail if I am overcommitting, and tell me what is the reason. Use empathetic voice and tone.
         These are the commitments I created for myself from the ${formatDate(fourWeeksAgo)} until the ${formatDate(referenceDate)}.
-        I was not able to finish these commitments on time:
+
+        ${hasOverdueCommitments ? 'I was not able to finish these commitments on time:' : 'I did not have any overdue commitments.'}
         ${groupedCommitments.overdue.map((commitment) => `I committed to "${commitment.title}" by ${commitment.doneBy}, and ${commitment.doneAt ? `I got it done on the ${commitment.doneAt}` : 'I could not get it done yet'};`)}.
-        I could finish these commitments on time:
+
+        ${hasOnTimeCommitments ? 'I finished these commitments on time:' : 'I did not finish any commitment on time.'}
         ${groupedCommitments.onTime.map((commitment) => `I committed to "${commitment.title}" by ${commitment.doneBy}, and I got it done on the ${commitment.doneAt};`)}.
     `;
 }
