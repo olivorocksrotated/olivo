80ce3b7560406da48a63a2d7562f5045190f36ce
Lean <leian1306@gmail.com>
2023-12-29 22:22:52 +0100

    middleware: add some more info and missing property

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,19 @@
 export { auth as middleware } from './config/auth';
 
 export const config = {
-    matcher: ['/((?!signin|_next/static|_next/image|favicon.ico|api/inngest).*)'],
-    pages: {
-        signIn: '/signin'
-    }
+    matcher: [
+        /*
+        * Match all request paths except for the ones starting with:
+        * - signin (signin page)
+        * - _next/static (static files)
+        * - _next/image (image optimization files)
+        * - favicon.ico (favicon file)
+        * - api/inngest (events api)
+        */
+        '/((?!signin|_next/static|_next/image|favicon.ico|api/inngest).*)'
+    ],
+    missing: [
+        { type: 'header', key: 'next-router-prefetch' },
+        { type: 'header', key: 'purpose', value: 'prefetch' }
+    ]
 };
